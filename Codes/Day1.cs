eb2ad45d34d537d436f6ee50c8c3d5a79873a115
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2016-12-20 09:44:53 +0000

    Day 1 Files
    Method stub and test case

---
+++ Codes/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Codes
+{
+    public class Day1
+    {
+        public static int Distance(string input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tests/Day1Fixture.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode.Codes;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day1Fixture
+    {
+        [Test]
+        public void R2_L3()
+        {
+            var result = Day1.Distance("R2, L3");
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void R2_R2_R2()
+        {
+            var result = Day1.Distance("R2, R2, R2");
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void R5_L5_R5_R3()
+        {
+            var result = Day1.Distance("R5, L5, R5, R3");
+            Assert.AreEqual(12, result);
+        }
+    }
+}
