4156e1bb4755e4c815d34ff45bd6ba43aca71495
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2016-12-21 18:35:37 +0000

    Added class structure and data for day 3

---
+++ Tests/Day3Fixture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day3Fixture
+    {
+    }
+}
