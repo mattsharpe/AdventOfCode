546d44c1808609c63a61482191aaafef292e7e28
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2016-12-23 11:31:44 +0000

    Part on complete.

---
+++ Solutions/Day7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode.Solutions
+{
+    public class Day7
+    {
+        private Regex _abba = new Regex(@"(\w)(\w)\2\1(?!\])");
+        private Regex _abbaInsideHyperNet = new Regex(@"(\w)(\w)\2\1\w*(?=\])");
+
+        public bool SupportsTLS(string input)
+        {
+            //look for Autonomous Bridge Bypass Annotation outside of hypernet sequences
+            if (_abbaInsideHyperNet.IsMatch(input)) return false;
+
+            var abbaMatches = _abba.Matches(input);
+            if (abbaMatches.Count == 0) return false;
+
+            //if we've matched and it's all the same letter then discard.
+            foreach (Match match in abbaMatches)
+            {
+                if (match.Groups[1].Value == match.Groups[2].Value) return false;
+            }
+            return true;
+        }
+
+        public int Part1()
+        {
+            var data = FileReader.ReadFile("day7 ip addresses.txt");
+            return data.Where(SupportsTLS).Count();
+        }
+    }
+}
---
+++ Tests/Day7Fixture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Solutions;
+using AdventOfCode.Utilities;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day7Fixture
+    {
+        private Day7 _day7 = new Day7();
+
+        [Test]
+        public void Sample1()
+        {
+            var input = "abba[mnop]qrst";
+            var result = _day7.SupportsTLS(input);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            var input = "abcd[bddb]xyyx";
+            var result = _day7.SupportsTLS(input);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Sample3()
+        {
+            var input = "aaaa[qwer]tyui";
+            var result = _day7.SupportsTLS(input);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Sample4()
+        {
+            var input = "ioxxoj[asdfgh]zxcvbn";
+            var result = _day7.SupportsTLS(input);
+            Assert.IsTrue(result);
+        }
+        
+        [Test]
+        public void Part1()
+        {
+            var result = _day7.Part1();
+            Assert.AreEqual(105, result);
+        }
+    }
+}
