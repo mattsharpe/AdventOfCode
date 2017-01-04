fee7f2e2dc95b8de0ea243e20e91c8db5945312c
mattsharpe <mattsharpe@users.noreply.github.com>
2017-01-04 17:50:25 +0000

    Feature/day8 (#9)
    * First changes for day 8
    
    * Day 8 part 1 complete.
    
    * Added an ouput to visualise the 'Tiny Display'

--- Solutions/Day4.cs
+++ Solutions/Day4.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Linq;
 using System.Text;
 using AdventOfCode.Utilities;
 
---
+++ Tests/Day8Fixture.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using AdventOfCode.Solutions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day8Fixture
+    {
+        private Day8 _day8;
+
+        [SetUp]
+        public void Setup()
+        {
+            _day8 = new Day8();    
+        }
+
+        [Test]
+        public void DisplayIsInitialisedToFalse()
+        {
+            Assert.AreEqual(0, _day8.ActivePixels);
+        }
+
+        [Test]
+        public void ThreeByTwoRectangle()
+        {
+            _day8.Rectangle("rect 3x2");
+            Assert.AreEqual(6, _day8.ActivePixels);
+
+            var display = _day8.ToString();
+            var sb = new StringBuilder();
+
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+
+            Assert.AreEqual(sb.ToString(), display);
+        }
+
+        [Test]
+        public void ThreeByThreeRectangle()
+        {
+            _day8.Rectangle("rect 3x3");
+            Assert.AreEqual(9, _day8.ActivePixels);
+
+            var display = _day8.ToString();
+            var sb = new StringBuilder();
+
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+
+            Assert.AreEqual(sb.ToString(),display);
+        }
+
+        [Test]
+        public void ThirtyFiveByOneRectangle()
+        {
+            _day8.Rectangle("rect 35x1");
+            Assert.AreEqual(35, _day8.ActivePixels);
+
+            var display = _day8.ToString();
+            var sb = new StringBuilder();
+
+            sb.AppendLine("###################################...............");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+
+            Assert.AreEqual(sb.ToString(),display);
+        }
+
+        [Test]
+        public void Column1X1()
+        {
+            _day8.ProcessInstruction("rect 3x3");
+
+            var display = _day8.ToString();
+            var sb = new StringBuilder();
+
+            _day8.ProcessInstruction("rotate column x=1 by 1");
+            sb.Clear();
+
+            sb.AppendLine("#.#...............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine(".#................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+
+            Assert.AreEqual(sb.ToString(), _day8.ToString());
+        }
+
+        [Test]
+        public void Row1X1()
+        {
+            _day8.ProcessInstruction("rect 3x3");
+
+            var sb = new StringBuilder();
+
+            _day8.ProcessInstruction("rotate row y=1 by 1");
+            sb.Clear();
+
+            sb.AppendLine("###...............................................");
+            sb.AppendLine(".###..............................................");
+            sb.AppendLine("###...............................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+            sb.AppendLine("..................................................");
+
+            Assert.AreEqual(sb.ToString(), _day8.ToString());
+        }
+
+        [Test]
+        public void TestData()
+        {
+            _day8.Part1();
+            Assert.AreEqual(116,_day8.ActivePixels);
+        }
+    }
+}
