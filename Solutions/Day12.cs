e6b82dd2b78aea9edbe1d99e1e4883d02c3a7d1f
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2017-01-17 01:31:18 +0000

    Started day 12

---
+++ Solutions/Day12.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode.Solutions
+{
+    class Day12
+    {
+        public int A;
+        public int B;
+        public int C;
+        public int D;
+
+        public void ProcessInstruction(string instruction)
+        {
+            if (instruction.StartsWith("inc"))
+            {
+                var details = instruction.Replace("inc ", "");
+                Shift(details, 1);
+            }
+            else if (instruction.StartsWith("dec"))
+            {
+                var details = instruction.Replace("dec ", "");
+                Shift(details, -1);
+            }
+        }
+
+        public void Shift(string register, int offset)
+        {
+            switch (register)
+            {
+                case "a":
+                    A += offset;
+                    break;
+                case "b":
+                    B+= offset;
+                    break;
+                case "c":
+                    C+= offset;
+                    break;
+                case "d":
+                    D+= offset;
+                    break;
+                default:
+                    throw new Exception("Not a valid instruction " + register);
+            }
+        }
+
+        public void Part1()
+        {
+            var instructions = FileReader.ReadFile("day12 assembunny.txt");
+        }
+    }
+}
---
+++ Tests/Day12Fixture.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Solutions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    class Day12Fixture
+    {
+        private Day12 _day12;
+
+        [SetUp]
+        public void Setup()
+        {
+            _day12 = new Day12();
+        }
+
+        [Test]
+        public void SampleData()
+        {
+            string[] instructions =
+            {
+                "cpy 41 a",
+                "inc a",
+                "inc a",
+                "dec a",
+                "jnz a 2",
+                "dec a"
+            };
+
+            foreach (var instruction in instructions)
+            {
+                _day12.ProcessInstruction(instruction);
+            }
+
+            Assert.AreEqual(42, _day12.A);
+        }
+
+        [Test]
+        public void Increment()
+        {
+            Assert.AreEqual(0, _day12.A);
+            _day12.ProcessInstruction("inc a");
+            Assert.AreEqual(1, _day12.A);
+
+            Assert.AreEqual(0, _day12.B);
+            _day12.ProcessInstruction("inc b");
+            Assert.AreEqual(1, _day12.B);
+
+            Assert.AreEqual(0, _day12.C);
+            _day12.ProcessInstruction("inc c");
+            Assert.AreEqual(1, _day12.C);
+
+            Assert.AreEqual(0, _day12.D);
+            _day12.ProcessInstruction("inc d");
+            Assert.AreEqual(1, _day12.D);
+        }
+
+        [Test]
+        public void Decrement()
+        {
+            _day12.A = _day12.B = _day12.C = _day12.D = 10;
+            Assert.AreEqual(10, _day12.A);
+            _day12.ProcessInstruction("dec a");
+            Assert.AreEqual(9, _day12.A);
+
+            Assert.AreEqual(10, _day12.B);
+            _day12.ProcessInstruction("dec b");
+            Assert.AreEqual(9, _day12.B);
+
+            Assert.AreEqual(10, _day12.C);
+            _day12.ProcessInstruction("dec c");
+            Assert.AreEqual(9, _day12.C);
+
+            Assert.AreEqual(10, _day12.D);
+            _day12.ProcessInstruction("dec d");
+            Assert.AreEqual(9, _day12.D);
+        }
+    }
+}
