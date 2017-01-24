445730dc2596972e688e0d58f527ad9cf9775980
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2017-01-24 08:54:22 +0000

    Day 15 Part 1 and 2 done.

---
+++ Tests/Day15Fixture.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Solutions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day15Fixture
+    {
+        private Day15 _day15;
+            
+
+        [SetUp]
+        public void Setup()
+        {
+            _day15 = new Day15();
+        }
+
+        [Test]
+        public void SampleData()
+        {
+            var sculpture = new List<Disc>
+            {
+                new Disc {Positions = 5, StartingPosition = 4 }, //t + 1 % 5
+                new Disc {Positions = 2, StartingPosition = 1}, //t + 2 % 2
+            };
+
+            _day15.Sculpture = sculpture;
+            var result = _day15.Part1();
+            
+            Assert.AreEqual(5, result);
+            Console.WriteLine(result);
+            
+        }
+
+        [Test]
+        public void Part1()
+        {
+            /*
+            Disc #1 has 5 positions; at time=0, it is at position 2.
+            Disc #2 has 13 positions; at time=0, it is at position 7.
+            Disc #3 has 17 positions; at time=0, it is at position 10.
+            Disc #4 has 3 positions; at time=0, it is at position 2.
+            Disc #5 has 19 positions; at time=0, it is at position 9.
+            Disc #6 has 7 positions; at time=0, it is at position 0.
+            */
+            var sculpture = new List<Disc>
+            {
+                new Disc {Positions = 5, StartingPosition = 2 },
+                new Disc {Positions = 13, StartingPosition = 7 },
+                new Disc {Positions = 17, StartingPosition = 10 },
+                new Disc {Positions = 3, StartingPosition = 2 },
+                new Disc {Positions = 19, StartingPosition = 9 },
+                new Disc {Positions = 7, StartingPosition = 0 },
+            };
+
+            _day15.Sculpture = sculpture;
+            var result = _day15.Part1();
+
+            Assert.AreEqual(148737, result);
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var sculpture = new List<Disc>
+            {
+                new Disc {Positions = 5, StartingPosition = 2 },
+                new Disc {Positions = 13, StartingPosition = 7 },
+                new Disc {Positions = 17, StartingPosition = 10 },
+                new Disc {Positions = 3, StartingPosition = 2 },
+                new Disc {Positions = 19, StartingPosition = 9 },
+                new Disc {Positions = 7, StartingPosition = 0 },
+                new Disc {Positions = 11, StartingPosition = 0 },
+            };
+
+            _day15.Sculpture = sculpture;
+            var result = _day15.Part1();
+
+            Assert.AreEqual(2353212, result);
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Tests/Day16Fixture.cs
@@ -0,0 +1,64 @@
+﻿using AdventOfCode.Solutions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day16Fixture
+    {
+        private Day16 _day16;
+
+        [SetUp]
+        public void Setup()
+        {
+            _day16 = new Day16();
+        }
+
+        [Test]
+        public void RandomData()
+        {
+            Assert.AreEqual("100",_day16.RandomData("1"));
+            Assert.AreEqual("001",_day16.RandomData("0"));
+            Assert.AreEqual("11111000000", _day16.RandomData("11111"));
+            Assert.AreEqual("1111000010100101011110000", _day16.RandomData("111100001010"));
+        }
+
+        [Test]
+        public void Checksum()
+        {
+            var result = _day16.Checksum("110010110100");
+            Assert.AreEqual("100", result);
+        }
+
+        [Test]
+        public void SampleData()
+        {
+            var target = 20;
+            var input = "10000";
+
+            string checksum =_day16.Part1(target, input);
+            Assert.AreEqual("01100", checksum);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var target = 272;
+            var input = "11011110011011101";
+
+            string checksum =_day16.Part1(target, input);
+            Assert.AreEqual("00000100100001100", checksum);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var target = 35651584;
+            var input = "11011110011011101";
+
+            string checksum =_day16.Part1(target, input);
+            Assert.AreEqual("00011010100010010", checksum);
+        }
+
+    }
+}
