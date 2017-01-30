b557e795e1c4ee51466399f01952d3d3d76fefae
Matt Sharpe <matthew.sharpe@sword-activerisk.com>
2017-01-30 17:33:03 +0000

    Started Day 19

--- Solutions/Day16.cs
+++ Solutions/Day16.cs
@@ -1,9 +1,7 @@
 ﻿
 
-using System;
 using System.Linq;
 using System.Text;
-using NUnit.Framework;
 
 namespace AdventOfCode.Solutions
 {
---
+++ Tests/Day18Fixture.cs
@@ -0,0 +1,78 @@
+﻿using AdventOfCode.Solutions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    class Day18Fixture
+    {
+        private Day18 _day18;
+        //.^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....
+
+        [SetUp]
+        public void Setup()
+        {
+            _day18 = new Day18();
+        }
+
+        [Test]
+        public void IsTrap()
+        {
+            //Its left and center tiles are traps, but its right tile is not.
+            Assert.That(_day18.IsTrap("^", "^", "."));
+
+            //Its center and right tiles are traps, but its left tile is not.
+            Assert.That(_day18.IsTrap(".", "^", "^"));
+
+            // Only its left tile is a trap.
+            Assert.That(_day18.IsTrap("^", ".", "."));
+
+            // Only its left tile is a trap.
+            Assert.That(_day18.IsTrap(".", ".", "^"));
+
+            
+            Assert.IsFalse(_day18.IsTrap(".", ".", "."));
+            Assert.IsFalse(_day18.IsTrap(".", "^", "."));
+            Assert.IsFalse(_day18.IsTrap("^", "^", "^"));
+        }
+
+        [Test]
+        public void Sample1()
+        {
+            _day18.Process("..^^.", 3);
+            _day18.Print();
+            var result = _day18.CountSafe();
+
+            Assert.AreEqual(6,result);
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            _day18.Process(".^^.^.^^^^", 10);
+            _day18.Print();
+            var result = _day18.CountSafe();
+
+            Assert.AreEqual(38,result);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            _day18.Process(".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....", 40);
+            var result = _day18.CountSafe();
+
+            Assert.AreEqual(2013, result); //too low
+        }
+
+        [Test]
+        public void Part2()
+        {
+            _day18.Process(".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....", 400000);
+            //_day18.Print();
+            long result = _day18.CountSafe();
+
+            Assert.AreEqual(20006289, result); //too low
+        }
+    }
+}
