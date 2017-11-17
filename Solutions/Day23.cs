09d924ac168f1ab859e78afa410cb524a271d937
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2017-11-17 17:32:16 +0000

    Completed Day 23 Parts 1 & 2

--- Solutions/Day23.cs
+++ Solutions/Day23.cs
@@ -1,14 +1,19 @@
 ﻿/*
  * --- Day 23: Safe Cracking ---
 
-This is one of the top floors of the nicest tower in EBHQ. The Easter Bunny's private office is here, complete with a safe hidden behind a painting, and who wouldn't hide a star in a safe behind a painting?
+This is one of the top floors of the nicest tower in EBHQ. The Easter Bunny's private office is here, complete with a safe hidden behind a painting, 
+and who wouldn't hide a star in a safe behind a painting?
 
-The safe has a digital screen and keypad for code entry. A sticky note attached to the safe has a password hint on it: "eggs". The painting is of a large rabbit coloring some eggs. You see 7.
+The safe has a digital screen and keypad for code entry. A sticky note attached to the safe has a password hint on it: "eggs". 
+The painting is of a large rabbit coloring some eggs. You see 7.
 
-When you go to type the code, though, nothing appears on the display; instead, the keypad comes apart in your hands, apparently having been smashed. Behind it is some kind of socket - one that matches a connector in your prototype computer! You pull apart the smashed keypad and extract the logic circuit, plug it into your computer, and plug your computer into the safe.
+When you go to type the code, though, nothing appears on the display; instead, the keypad comes apart in your hands, apparently having been smashed. 
+Behind it is some kind of socket - one that matches a connector in your prototype computer! You pull apart the smashed keypad and extract the logic circuit, 
+plug it into your computer, and plug your computer into the safe.
 
 Now, you just need to figure out what output the keypad would have sent to the safe. You extract the assembunny code from the logic chip (your puzzle input).
-The code looks like it uses almost the same architecture and instruction set that the monorail computer used! You should be able to use the same assembunny interpreter for this as you did there, but with one new instruction:
+The code looks like it uses almost the same architecture and instruction set that the monorail computer used! 
+You should be able to use the same assembunny interpreter for this as you did there, but with one new instruction:
 
 tgl x toggles the instruction x away (pointing at instructions like jnz does: positive means forward; negative means backward):
 
@@ -17,7 +22,8 @@ For two-argument instructions, jnz becomes cpy, and all other two-instructions b
 The arguments of a toggled instruction are not affected.
 If an attempt is made to toggle an instruction outside the program, nothing happens.
 If toggling produces an invalid instruction (like cpy 1 2) and an attempt is later made to execute that instruction, skip it instead.
-If tgl toggles itself (for example, if a is 0, tgl a would target itself and become inc a), the resulting instruction is not executed until the next time it is reached.
+If tgl toggles itself (for example, if a is 0, tgl a would target itself and become inc a), 
+    the resulting instruction is not executed until the next time it is reached.
 For example, given this program:
 
 cpy 2 a
@@ -41,14 +47,139 @@ What value should be sent to the safe?
 
  */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using AdventOfCode.Utilities;
+using NUnit.Framework.Constraints;
 
 namespace AdventOfCode.Solutions
 {
     class Day23
     {
+        public int A;
+        public int B;
+        public int C;
+        public int D;
+
+        public void ProcessInstructions(string[] instructions)
+        {
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                var instruction = instructions[i];
+                
+                var details = instruction.Substring(4);
+                switch (instruction.Substring(0, 3))
+                {
+                    case "inc":
+                        Shift(details, 1);
+                        break;
+                    case "dec":
+                        Shift(details, -1);
+                        break;
+                    case "cpy":
+                        Copy(details.Split(' ')[0], details.Split(' ')[1]);
+                        break;
+                    case "jnz":
+                        var modifier = Jump(details.Split(' ')[0], details.Split(' ')[1]);
+                        i += modifier;
+                        break;
+                    case "tgl":
+                        Toggle(details, instructions, i);
+                        break;
+                    case "nop":
+                        break;
+                    case "mul":
+                        Multiply(details);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Instruction not supported " + instruction);
+                }
+            }
+        }
+
+        private void Multiply(string details)
+        {
+            var parts = details.Split(' ');
+            var field = GetField(parts[2]);
+            
+            field.SetValue(this, GetValue(parts[0]) * GetValue(parts[1]));
+        }
+
+        private void Toggle(string details, string[] instructions, int currentLocation)
+        {
+            //If tgl toggles itself (for example, if a is 0, tgl a would target itself and become inc a), 
+            //the resulting instruction is not executed until the next time it is reached.
+            if (GetValue(details) == 0) return; 
+
+            var target = GetValue(details) + currentLocation;
+
+            if (target >= instructions.Length) return;
+
+            var targetInstruction = instructions[target].Substring(0,3);
+            switch (targetInstruction)
+            {
+                case "inc":
+                    instructions[target] = instructions[target].Replace("inc", "dec");
+                    break;
+                case "dec":
+                    instructions[target] = instructions[target].Replace("dec", "inc");
+                    break;
+                case "jnz":
+                    instructions[target] = instructions[target].Replace("jnz", "cpy");
+                    break;
+                case "cpy":
+                    instructions[target] = instructions[target].Replace("cpy", "jnz");
+                    break;
+                case "tgl":
+                    instructions[target] = instructions[target].Replace("tgl", "inc");
+                    break;
+                default:
+                    throw new InvalidOperationException("Don't know how to toggle that " + targetInstruction);
+            }
+        }
+
+        public int Jump(string x, string distance)
+        {
+            var control = GetValue(x);
+            if (control == 0) return 0;
+            var modifier = GetValue(distance);
+            return modifier - 1;
+
+        }
+
+        public void Shift(string register, int offset)
+        {
+            var field = GetField(register);
+            var currentValue = Convert.ToInt32(field.GetValue(this));
+            field.SetValue(this, currentValue + offset);
+        }
+
+        public void Copy(string input, string target)
+        {
+            var value = GetValue(input);
+            GetField(target).SetValue(this, value);
+
+        }
+
+        /// <summary>
+        /// Returns the int value of the instruction, or the value in the register specified
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int GetValue(string input)
+        {
+            int value;
+            var isValue = int.TryParse(input, out value);
+
+            if (!isValue)
+            {
+                value = Convert.ToInt32(GetField(input).GetValue(this));
+            }
+            return value;
+        }
+        
+        private FieldInfo GetField(string name)
+        {
+            return GetType().GetField(name.Trim().ToUpper());
+        }
     }
 }
---
+++ Tests/Day23Fixture.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Solutions;
+using AdventOfCode.Utilities;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    class Day23Fixture
+    {
+        private Day23 _day23 = new Day23();
+
+        [Test]
+        public void SampleData()
+        {
+            var input = new[]{"cpy 2 a",
+                "tgl a",
+                "tgl a",
+                "tgl a",
+                "cpy 1 a",
+                "dec a",
+                "dec a"};
+
+            _day23.ProcessInstructions(input);
+
+            Console.WriteLine(_day23.A);
+            Assert.AreEqual(3, _day23.A);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var input = FileReader.ReadFile("day 23.txt");
+
+            _day23.A = 7;
+            _day23.ProcessInstructions(input);
+
+            Assert.AreEqual(11500, _day23.A);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var instructions = new[]
+            {
+                "cpy a b",
+                "dec b",
+
+                "nop ", //"cpy a d",
+                "nop ", //"cpy 0 a",
+                "nop ", //"cpy b c",
+                "nop ", //"inc a",
+                "nop ", //"dec c",
+                "nop ", //"jnz c -2",
+                "nop ", //"dec d",
+                "mul b a a", //"jnz d -5",
+
+                "dec b",
+                "cpy b c",
+                "cpy c d",
+                "dec d",
+                "inc c",
+                "jnz d -2",
+                "tgl c",
+                "cpy -16 c",
+                "jnz 1 c",
+                "cpy 85 c",
+                "jnz 76 d",
+                "inc a",
+                "inc d",
+                "jnz d -2",
+                "inc c",
+                "jnz c -5"
+            };
+
+            _day23.A = 12;
+            _day23.ProcessInstructions(instructions);
+
+            Assert.AreEqual(479008060, _day23.A);
+        }
+    }
+}
