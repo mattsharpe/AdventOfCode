9851644f098e358365361c98510afbb33523ceb0
Matt Sharpe <matt@mattsharpe.co.uk>
2017-11-27 23:38:18 +0000

    Day 3 Part 1

---
+++ Advent2015/Solutions/Day3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent2015.Solutions
+{
+    class Day3
+    {
+        public Day3()
+        {
+            X = 0;
+            Y = 0;
+            Visited = new HashSet<Tuple<int, int>>
+            {
+                new Tuple<int, int>(X, Y)
+            };
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public HashSet<Tuple<int, int>> Visited;
+
+        public void ProcessInstructions(string input)
+        {
+            foreach (var instruction in input)
+            {
+                switch (instruction)
+                {
+                    case '<':
+                        X--;
+                        break;
+                    case '>':
+                        X++;
+                        break;
+                    case '^':
+                        Y--;
+                        break;
+                    case 'v':
+                        Y++;
+                        break;
+                    default:
+                        throw new Exception("Uknown instruction");
+                }
+
+                Visited.Add(new Tuple<int,int>(X, Y));
+            }
+        }
+    }
+}
---
+++ Advent2015/Tests/Day3Fixture.cs
@@ -0,0 +1,55 @@
+﻿using Advent2015.Solutions;
+using Advent2015.Utilities;
+using NUnit.Framework;
+
+namespace Advent2015.Tests
+{
+    [TestFixture]
+    class Day3Fixture
+    {
+        private Day3 _day3 = new Day3();
+
+        [SetUp]
+        public void Setup()
+        {
+            _day3 = new Day3();
+        }
+
+        [Test]
+        public void SampleData()
+        {
+            string input = ">";
+            _day3.ProcessInstructions(input);
+            var visited = _day3.Visited.Count;
+            Assert.AreEqual(2, visited);
+        }
+
+        [Test]
+        public void SampleData_Square()
+        {
+            string input = "^>v<";
+            _day3.ProcessInstructions(input);
+            var visited = _day3.Visited.Count;
+            Assert.AreEqual(4, visited);
+        }
+
+        [Test]
+        public void SampleData_Bouncing()
+        {
+            string input = "^v^v^v^v^v";
+            _day3.ProcessInstructions(input);
+            var visited = _day3.Visited.Count;
+            Assert.AreEqual(2, visited);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var input = FileReader.ReadFile("day3.txt")[0];
+            _day3.ProcessInstructions(input);
+
+            var visited = _day3.Visited.Count;
+            Assert.AreEqual(2572, visited);
+        }
+    }
+}
