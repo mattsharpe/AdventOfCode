b8cab3cde2ff91d659cf716febd2f12bbfed9510
Matt Sharpe <matt@mattsharpe.co.uk>
2017-11-28 12:55:49 +0000

    Day5 - Part 1

---
+++ Advent2015/Tests/Day5Fixture.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
+using NUnit.Framework;
+
+namespace Advent2015.Tests
+{
+    [TestFixture]
+    public class Day5Fixture
+    {
+        private Day5 _day5 = new Day5();
+
+        [Test]
+        public void Nice_ugknbfddgicrmopn()
+        {
+            //ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), 
+            //and none of the disallowed substrings.
+            Assert.That(_day5.IsNice("ugknbfddgicrmopn"));
+        }
+
+        [Test]
+        public void Nice_aaa()
+        {
+            //aaa is nice because it has at least three vowels and a double letter, 
+            // even though the letters used by different rules overlap.
+        Assert.That(_day5.IsNice("aaa"));
+        }
+
+        [Test]
+        public void Naughty_jchzalrnumimnmhp()
+        {
+            //jchzalrnumimnmhp is naughty because it has no double letter.
+            Assert.False(_day5.IsNice("jchzalrnumimnmhp"));
+        }
+
+        [Test]
+        public void Naughty_haegwjzuvuyypxyu()
+        {
+            //haegwjzuvuyypxyu is naughty because it contains the string xy.
+            Assert.False(_day5.IsNice("haegwjzuvuyypxyu"));
+        }
+
+        [Test]
+        public void Naughty_dvszwmarrgswjxmb()
+        {
+            //dvszwmarrgswjxmb is naughty because it contains only one vowel.
+            Assert.False(_day5.IsNice("dvszwmarrgswjxmb"));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var input = FileReader.ReadFile("day5.txt");
+            Assert.AreEqual(238, input.Count(x => _day5.IsNice(x)));
+        }
+    }
+}
