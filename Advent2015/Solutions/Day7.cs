b87d35b64501cbafd19936b2450ebde700ec6605
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-01 09:24:40 +0000

    2015 Day 7, start

---
+++ Advent2015/Solutions/Day7.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2015.Solutions
+{
+    class Day7
+    {
+        public void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Dictionary<string, ushort> _cache = default;
+        private Dictionary<string, string> _wires = default;
+
+        public ushort And(string a, string b)
+        {
+            return Convert.ToUInt16((GetValue(a) & GetValue(b)));
+        }
+
+        public ushort Or(string a, string b)
+        {
+            return Convert.ToUInt16((GetValue(a) | GetValue(b)));
+        }
+
+        public ushort Not(string a)
+        {
+            return Convert.ToUInt16(~GetValue(a));
+        }
+
+        public ushort LShift(string s, string shift)
+        {
+            GetValue(s);
+            GetValue(shift);
+            return  (ushort)(GetValue(s) << GetValue(shift));
+
+        }
+
+        private ushort GetValue(string value)
+        {
+            ushort temp;
+            if (UInt16.TryParse(value, out temp))
+            {
+                return temp;
+            }
+            else
+            {
+                return _cache[value];
+            }
+
+        }
+
+        public void Solve(string[] instructions)
+        {
+            _wires = instructions.ToList().ToDictionary(k => k.Split(' ').Last());
+            foreach (var instruction in instructions)
+            {
+                var split = instruction.Split(' ');
+                Console.WriteLine(instruction);
+                var gate = new LogicGate();
+                switch (split.Length)
+                {
+                    //123 -> a
+                    case 3:
+                        gate.Left = split[0];
+                        gate.Right = split[2];
+                        break;
+                    
+                }
+            }
+        }
+    }
+
+    public class LogicGate
+    {
+        public string Left { get; set; }
+        public string Right { get; set; }
+        public ushort Value { get; set; }
+    }
+
+    public enum LogicOperator
+    {
+        And,
+        Or,
+        LShift,
+        RShift,
+        Not
+    }
+}
---
+++ Advent2015/Tests/Day7Fixture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
+using NUnit.Framework;
+
+namespace Advent2015.Tests
+{
+    [TestFixture]
+    class Day7Fixture
+    {
+        private Day7 _day7 = new Day7();
+
+        private string[] _sample = {
+            "123 -> x",
+            "456 -> y",
+            "x AND y -> d",
+            "x OR y -> e",
+            "x LSHIFT 2 -> f",
+            "y RSHIFT 2 -> g",
+            "NOT x -> h",
+            "NOT y -> i"
+        };
+
+        [Test]
+        public void Part1()
+        {
+            _day7.Part1();
+        }
+
+        [Test]
+        public void And()
+        {
+            Assert.AreEqual(72, _day7.And("123","456"));    
+        }
+        
+        [Test]
+        public void Or()
+        {
+            Assert.AreEqual(507, _day7.Or("123","456"));    
+        }
+
+        [Test]
+        public void Not()
+        {
+            Assert.AreEqual(65412, _day7.Not("123"));    
+        }
+
+        [Test]
+        public void LShift()
+        {
+            Assert.AreEqual(492, _day7.LShift("123", "2"));    
+        }
+
+        [Test]
+        public void SampleData()
+        {
+            _day7.Solve(_sample);
+        }
+    }
+}
