52660183b233eb54fa4bbb642439ba966fc14fe6
mattsharpe <mattsharpe@users.noreply.github.com>
2017-12-03 17:02:38 +0000

    2005 day7 (#13)
    * 2015 Day 7, start
    
    * Crazy Reflection idea
    
    * Day 7 work
    
    * Day 7 done. Compiling a custom class based on the given inputs. Elegant or insane, not sure...?

---
+++ Advent2015/Solutions/Day7.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace Advent2015.Solutions
+{
+    class Day7
+    {
+
+        public string Transpile(string instruction)
+        {
+            var template = "public const ushort {0} = unchecked((ushort)({1}));";
+            instruction = instruction.Replace("NOT ", "~").Replace("OR", "|").Replace("AND", "&")
+                .Replace("LSHIFT", "<<").Replace("RSHIFT", ">>").Replace("->","=")
+                .Replace("as","@as").Replace("if","@if").Replace("do", "@do").Replace("is", "@is").Replace("in", "@in");
+
+            var parts = instruction.Split('=');
+            return string.Format(template, parts[1].Trim(), parts[0].Trim());
+        }
+
+        public ushort BuildSolver(string[] instructions, string register)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"public class Logic{register} {{");
+            
+            foreach (var instruction in instructions)
+            {
+                sb.AppendLine(Transpile(instruction));
+            }
+             
+            sb.AppendLine("public ushort GetValue(){");
+            sb.AppendLine($"return {register};");
+            sb.AppendLine(" }");
+            sb.AppendLine("}");
+            //Console.WriteLine(sb.ToString());
+            var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } });
+            var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, $"Advent{register}.dll", true);
+
+            var results = csc.CompileAssemblyFromSource(parameters, sb.ToString());
+            var type = results.CompiledAssembly.GetType($"Logic{register}");
+            dynamic logic = Activator.CreateInstance(type);
+            
+            return (ushort)logic.GetValue();
+        }
+    }
+    
+}
---
+++ Advent2015/Tests/Day7Fixture.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
+using NUnit.Framework;
+
+namespace Advent2015.Tests
+{
+    [TestFixture]
+    class Day7Fixture
+    {
+        private Day7 _day7 = new Day7();
+
+        private string[] _sample = {
+            "123 -> x",
+            "456 -> y",
+            "x AND y -> d",
+            "x OR y -> e",
+            "x LSHIFT 2 -> f",
+            "y RSHIFT 2 -> g",
+            "NOT x -> h",
+            "NOT y -> i"
+        };
+
+        [TestCase("123 -> x", "public const ushort x = unchecked((ushort)(123));")]
+        [TestCase("456 -> y", "public const ushort y = unchecked((ushort)(456));")]
+        [TestCase("x AND y -> d", "public const ushort d = unchecked((ushort)(x & y));")]
+        [TestCase("x OR y -> e", "public const ushort e = unchecked((ushort)(x | y));")]
+        [TestCase("x LSHIFT 2 -> f", "public const ushort f = unchecked((ushort)(x << 2));")]
+        [TestCase("y RSHIFT 2 -> g", "public const ushort g = unchecked((ushort)(y >> 2));")]
+        [TestCase("NOT x -> h", "public const ushort h = unchecked((ushort)(~x));")]
+        [TestCase("NOT y -> i", "public const ushort i = unchecked((ushort)(~y));")]
+        public void Transpile(string input, string expected)
+        {
+            Assert.AreEqual(expected, _day7.Transpile(input));
+        }
+        
+        [TestCase("d", 72)]
+        [TestCase("e", 507)]
+        [TestCase("f", 492)]
+        [TestCase("g", 114)]
+        [TestCase("h", 65412)]
+        [TestCase("i", 65079)]
+        [TestCase("x", 123)]
+        [TestCase("y", 456)]
+        public void SampleData(string register, int expected)
+        {
+            Assert.AreEqual(expected, _day7.BuildSolver(_sample, register));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var result = _day7.BuildSolver(FileReader.ReadFile("day7.txt"),"a");
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var input = FileReader.ReadFile("day7.txt");
+            input[54] = "16076 -> b";
+            var result = _day7.BuildSolver(input,"a");
+            Console.WriteLine(result);
+        }
+        
+    }
+}
