aab5914fa08defe1b19b86987d3416927e986af8
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-03 18:59:15 +0000

    Day 8 done, bit of regex decoding and string replace encoding.

---
+++ Advent2015/Solutions/Day8.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2015.Solutions
+{
+    class Day8
+    {
+
+        public int DecodedLength(string line)
+        {
+            var match = Regex.Match(line, @"^""(\\x..|\\.|.)*""$");
+            return match.Groups[1].Captures.Count; // how many encoded things have we matched?
+        }
+
+        public int EncodedLength(string line)
+        {
+            return line.Replace("\\", "**").Replace("\"", "**").Length + 2;
+        }
+
+        public int Part1(string[] input)
+        {
+            return input.Sum(x => x.Length)- input.Sum(DecodedLength);
+        }
+
+        public int Part2(string[] input)
+        {
+            return input.Sum(EncodedLength) - input.Sum(x => x.Length);
+        }
+    }
+}
---
+++ Advent2015/Tests/Day8Fixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml.Schema;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
+using NUnit.Framework;
+
+namespace Advent2015.Tests
+{
+    [TestFixture]
+    class Day8Fixture
+    {
+        private Day8 _day8 = new Day8();
+        
+        [TestCase("\"\"", 2, 0)]
+        [TestCase("\"abc\"", 5, 3)]
+        [TestCase("\"aaa\\\"aaa\"", 10, 7)]
+        [TestCase("\"\\x27\"", 6, 1)]
+        public void Sample(string input, int lengthOfCode, int lengthOfString)
+        {
+            var result = _day8.DecodedLength(input);
+            Assert.AreEqual(lengthOfString, result);
+            Assert.AreEqual(lengthOfCode, input.Length);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var input = FileReader.ReadFile("day8.txt");
+            Assert.AreEqual(1342, _day8.Part1(input));
+        }
+
+        [TestCase("\"\"", 6)]
+        [TestCase("\"abc\"", 9)]
+        [TestCase("\"aaa\\\"aaa\"", 16)]
+        [TestCase("\"\\x27\"", 11)]
+        public void Sample2(string input, int lengthEncoded)
+        {
+            var result = _day8.EncodedLength(input);
+            Assert.AreEqual(lengthEncoded, result);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var input = FileReader.ReadFile("day8.txt");
+            Assert.AreEqual(2074, _day8.Part2(input));
+        }
+    }
+}
