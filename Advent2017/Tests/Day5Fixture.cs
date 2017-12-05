9e3fdc1a7626c1ca4b8b33676e1ca9b377d578e7
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-05 21:33:52 +0000

    Day 5 done, bit slow though.

---
+++ Advent2017/Tests/Day5Fixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Advent2017.Solutions;
+using Advent2017.Utilities;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    [TestFixture]
+    class Day5Fixture
+    {
+        private Day5 _day5 = new Day5();
+
+        private int[] _sample = {0, 3, 0, 1, -3,};
+
+        [Test]
+        public void Sample()
+        {
+            Assert.AreEqual(5, _day5.Solve(_sample));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var input = FileReader.ReadFile("day5.txt").Select(x => Convert.ToInt32(x)).ToArray();
+            Assert.AreEqual(355965, _day5.Solve(input));
+        }
+
+        [Test]
+        public void Sample_Part2()
+        {
+            Assert.AreEqual(10, _day5.SolvePart2(_sample));
+        }
+
+
+        [Test]
+        public void Part2()
+        {
+            var input = FileReader.ReadFile("day5.txt").Select(x => Convert.ToInt32(x)).ToArray();
+            Assert.AreEqual(26948068, _day5.SolvePart2(input));
+        }
+    }
+}
