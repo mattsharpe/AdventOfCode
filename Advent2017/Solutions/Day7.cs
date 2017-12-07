7e738fd976a1b477504447bbf06eddc0cceb6227
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-07 16:36:03 +0000

    Day 7 done. Hmmmmm recursion!

---
+++ Advent2017/Solutions/Day7.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2017.Solutions
+{
+    class Day7
+    {
+        public string FindBottomProgram(string[] sample)
+        {
+            BuildNetwork(sample);
+            return  _programs.Values.Single(x => x.Parent == null).Name;
+        }
+
+        private void BuildNetwork(string[] sample)
+        {
+            Regex matcher = new Regex(@"(\w+) \((\d+)\)(?: -> )?(.*)");
+            foreach (var s in sample)
+            {
+                var match = matcher.Match(s);
+                var prog = new Program
+                {
+                    Name = match.Groups[1].Value,
+                    Size = Convert.ToInt32(match.Groups[2].Value),
+                    ChildrenInput = match.Groups[3].Value,
+                    Children = new List<Program>()
+                };
+                _programs.Add(prog.Name, prog);
+            }
+
+            foreach (var kvp in _programs)
+            {
+                if (string.IsNullOrEmpty(kvp.Value.ChildrenInput)) continue;
+                var children = kvp.Value.ChildrenInput.Split(',');
+                foreach (var child in children)
+                {
+                    var parentNode = kvp.Value;
+                    var childNode = _programs[child.Trim()];
+                    childNode.Parent = kvp.Value;
+                    parentNode.Children.Add(childNode);
+                }
+            }
+        }
+
+        private Dictionary<string, Program> _programs = new Dictionary<string, Program>();
+
+        public int FindCorrectedWeight(string[] input)
+        {
+            BuildNetwork(input);
+            
+            var node = _programs.Values.Single(x => x.Parent == null);
+
+            return AdjustNodeToWeight(node, 0);
+        }
+
+        public int AdjustNodeToWeight(Program node, int targetWeight)
+        {
+            Console.WriteLine("Adjust node to weight for " + node.Name);
+            if (node.IsBalanced)
+            {
+                var delta = targetWeight - node.TotalSize;
+                return node.Size + delta;
+            }
+            //find the child node that is different to the others and recurse on that node.
+            var group = node.Children.GroupBy(x => x.TotalSize).OrderByDescending(x => x.Count());
+            var problemChild = group.Last().First();
+            var targetSize = group.First().Key;
+            return AdjustNodeToWeight(problemChild, targetSize);
+        }
+    }
+
+    class Program
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public Program Parent { get; set; }
+        public List<Program> Children { get; set; }
+        public string ChildrenInput { get; set; }
+
+        public bool IsBalanced
+        {
+            get { return Children.All(x => x.TotalSize == Children[0].TotalSize); }
+        }
+
+        public int TotalSize
+        {
+            get { return Children.Sum(x => x.TotalSize) + Size; }
+        }
+    }
+}
---
+++ Advent2017/Tests/Day7Fixture.cs
@@ -0,0 +1,58 @@
+﻿using Advent2017.Solutions;
+using Advent2017.Utilities;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    class Day7Fixture
+    {
+        private Day7 _day7 = new Day7();
+
+        [SetUp]
+        public void Setup()
+        {
+            _day7 = new Day7();
+        }
+
+        public string[] _sample =
+        {
+            "pbga (66)",
+            "xhth (57)",
+            "ebii (61)",
+            "havc (66)",
+            "ktlj (57)",
+            "fwft (72) -> ktlj, cntj, xhth",
+            "qoyq (66)",
+            "padx (45) -> pbga, havc, qoyq",
+            "tknk (41) -> ugml, padx, fwft",
+            "jptl (61)",
+            "ugml (68) -> gyxo, ebii, jptl",
+            "gyxo (61)",
+            "cntj (57)"
+        };
+
+        [Test]
+        public void Sample()
+        {
+            Assert.AreEqual("tknk", _day7.FindBottomProgram(_sample));
+        }
+         
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual("hlhomy", _day7.FindBottomProgram(FileReader.ReadFile("day7.txt")));
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            Assert.AreEqual(60, _day7.FindCorrectedWeight(_sample));
+        }
+        
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(1505, _day7.FindCorrectedWeight(FileReader.ReadFile("day7.txt")));
+        }
+    }
+}
