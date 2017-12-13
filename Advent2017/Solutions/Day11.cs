52417c60f71ab823463bf9d5e3cbdf4a3e080349
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-13 20:44:18 +0000

    Day 11 - Hex Ed, fun with hex grids and co-ordinates. There's got to be a 'cooler' way to handle the instructions though; feels very procedural and nasty

---
+++ Advent2017/Solutions/Day11.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2017.Solutions
+{
+    class Day11
+    {
+        
+        //Thank you https://www.redblobgames.com/grids/hexagons/#distances
+        public int Distance(string directions, bool part2 = false)
+        {
+            int x=0, y=0, z = 0;
+            int furthestDistance = 0;
+            foreach (var direction in directions.Split(','))
+            {
+                switch (direction)
+                {
+                    case "n":
+                        y++;
+                        z--;
+                        break;
+                    case "ne":
+                        x++;
+                        z--;
+                        break;
+                    case "se":
+                        y--;
+                        x++;
+                        break;
+                    case "s":
+                        y--;
+                        z++;
+                        break;
+                    case "sw":
+                        x--;
+                        z++;
+                        break;
+                    case "nw":
+                        x--;
+                        y++;
+                        break;
+                }
+                if (CubicDistance(x, y, z) > furthestDistance)
+                    furthestDistance = CubicDistance(x, y, z);
+            }
+
+            return part2 ? furthestDistance : CubicDistance(x, y, z);
+            
+        }
+
+        private static int CubicDistance(int x, int y, int z)
+        {
+            return (Math.Abs(x - 0) + Math.Abs(y - 0) + Math.Abs(z - 0)) / 2;
+        }
+    }
+}
---
+++ Advent2017/Tests/Day11Fixture.cs
@@ -0,0 +1,33 @@
+﻿using Advent2017.Solutions;
+using Advent2017.Utilities;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    [TestFixture]
+    class Day11Fixture
+    {
+        private Day11 _day11 = new Day11();
+
+        [TestCase("ne,ne,ne",3)]
+        [TestCase("ne,ne,sw,sw", 0)]
+        [TestCase("ne,ne,s,s", 2)]
+        [TestCase("se,sw,se,sw,sw", 3)]
+        public void Sample(string directions, int distance)
+        {
+            Assert.AreEqual(distance, _day11.Distance(directions));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(715, _day11.Distance(FileReader.ReadFile("day11.txt")[0]));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(1512, _day11.Distance(FileReader.ReadFile("day11.txt")[0], true));
+        }
+    }
+}
