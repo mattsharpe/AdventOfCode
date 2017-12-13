0d7f3de6757777705eeca1e2ad262142b9784a27
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-13 00:38:25 +0000

    Day 10 - Knot Hash. Some nasty string processing and binary conversion. At least it wasn't MD5 again...

---
+++ Advent2017/Solutions/Day10.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Solutions
+{
+    class Day10
+    {
+        public List<int> List = Enumerable.Range(0, 256).ToList();
+        private int _currentPosition;
+        private int _skipLength;
+
+        public void ApplyInput(int[] input)
+        {
+            _currentPosition = 0;
+            _skipLength = 0;
+
+            //Then, for each length:
+            //Reverse the order of that length of elements in the list, starting with the element at the current position.
+            //Move the current position forward by that length plus the skip size.
+            //Increase the skip size by one.
+            //3, 4, 1, 5
+            RunRound(input);
+        }
+
+        private void RunRound(int[] input)
+        {
+            foreach (var length in input)
+            {
+                //start a new list from the current position, wrapping around
+                List = List.Skip(_currentPosition).Concat(List.Take(_currentPosition)).ToList();
+                //reverse the sublist by teh amount of the input and concat the rest of the lsit.
+                List = List.Take(length).Reverse().Concat(List.Skip(length)).ToList();
+                //put the list back to the way it was before we meddled with it
+                List = List.Skip(List.Count - _currentPosition).Concat(List.Take(List.Count - _currentPosition)).ToList();
+
+                _currentPosition = _currentPosition = (length + _skipLength + _currentPosition) % List.Count;
+
+                _skipLength++;
+            }
+        }
+
+        public string KnotHash(int[] bytes)
+        {
+            var input = bytes.Select(x => (int)x).Concat(new[] { 17, 31, 73, 47, 23 }).ToArray();
+
+            _currentPosition = 0;
+            _skipLength = 0;
+
+            foreach (var unused in Enumerable.Repeat(0,64))
+            {
+                RunRound(input);
+            }
+            
+            var denseHash = new List<int>();
+            
+
+            for (var x = 0; x < List.Count - 1; x += 16)
+            {
+                var hash = 0;
+
+                for (var i = 0; i < 16; i++)
+                {
+                    hash ^= List[x + i];
+                }
+
+                denseHash.Add(hash);
+            }
+
+            return denseHash.Aggregate(new StringBuilder(),(x,y) => x.Append($"{y:x2}")).ToString();
+        }
+    }
+}
---
+++ Advent2017/Tests/Day10Fixture.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Advent2017.Solutions;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    [TestFixture]
+    class Day10Fixture
+    {
+        private Day10 _day10;
+        private readonly int[] _puzzleInput = {63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24};
+        private string _part2 = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24";
+
+        [SetUp]
+        public void Setup()
+        {
+            _day10 = new Day10();
+        }
+
+        [Test]
+        public void Sample()
+        {
+            //list of 0,1,2,3,4
+            _day10.List = Enumerable.Range(0, 5).ToList();
+            int[] input = {3, 4, 1, 5};
+            _day10.ApplyInput(input);
+
+            Assert.AreEqual(12, _day10.List.Take(2).Aggregate((x,y)=> x*y));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            _day10.List = Enumerable.Range(0, 256).ToList();
+            _day10.ApplyInput(_puzzleInput);
+
+            Assert.AreEqual(4480, _day10.List.Take(2).Aggregate((x,y)=> x*y));
+        }
+
+        [TestCase("", "a2582a3a0e66e6e86e3812dcb672a272")]
+        [TestCase("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd")]
+        [TestCase("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d")]
+        [TestCase("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
+        public void Part2_Sample(string test, string expected)
+        {
+
+            Assert.AreEqual(expected,_day10.KnotHash(test.Select(x => (int)x).ToArray()));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual("c500ffe015c83b60fad2e4b7d59dabc4", _day10.KnotHash(_part2.Select(x => (int)x).ToArray()));
+        }
+    }
+}
