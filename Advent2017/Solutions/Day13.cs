446fbc2b0dcb3bc59bba9f2d23a3876fd4052900
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-20 12:22:53 +0000

    Day 13: Packet Scanners. Done. Math, urgh.

---
+++ Advent2017/Solutions/Day13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2017.Solutions
+{
+    class Day13
+    {
+        public int CalculateSeverity(string[] sample)
+        {
+            var layers = GetLayers(sample);
+            return layers.Where(layer => layer.Item1 % (2 * (layer.Item2 - 1)) == 0)
+                .Sum(layer => layer.Item1 * layer.Item2);
+        }
+
+        public int Severity(IEnumerable<Tuple<int,int>> layers)
+        {
+            return layers.Where(layer => layer.Item1 % (2 * (layer.Item2 - 1)) == 0)
+                .Sum(layer => layer.Item1 * layer.Item2);
+        }
+
+        private IEnumerable<Tuple<int, int>> GetLayers(string[] sample)
+        {
+            return sample.Select(x => new Tuple<int, int>
+            (
+                Convert.ToInt32(x.Split(':')[0]), //item1 = Range
+                Convert.ToInt32(x.Split(':')[1]) //item 2 = Depth
+            ));
+        }
+
+        public int CalculateDelay(string[] sample)
+        {
+            var layers = GetLayers(sample);
+
+            return Enumerable.Range(0, int.MaxValue).First(d => 
+                layers.All(s => (s.Item1 + d) % (2 * (s.Item2 - 1)) != 0));
+        }
+    }
+}
---
+++ Advent2017/Tests/Day13Fixture.cs
@@ -0,0 +1,44 @@
+﻿using Advent2017.Solutions;
+using Advent2017.Utilities;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    [TestFixture]
+    public class Day13Fixture
+    {
+        private string[] _sample =
+        {
+            "0: 3",
+            "1: 2",
+            "4: 4",
+            "6: 4"
+        };
+
+        private Day13 _day13 = new Day13();
+
+        [Test]
+        public void Sample()
+        {
+            Assert.AreEqual(24, _day13.CalculateSeverity(_sample));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(748, _day13.CalculateSeverity(FileReader.ReadFile("day13.txt")));
+        }
+
+        [Test]
+        public void Part2_Sample()
+        {
+            Assert.AreEqual(10, _day13.CalculateDelay(_sample));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(3873662, _day13.CalculateDelay(FileReader.ReadFile("day13.txt")));
+        }
+    }
+}
