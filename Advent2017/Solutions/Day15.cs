ade1b1eefd8ce2060ca407b9cee7d998cefa8e01
Matt Sharpe <matt@mattsharpe.co.uk>
2017-12-20 17:27:08 +0000

    Day 15: Dueling Generators. Likely some speed gains to be made here. Quite like the clean LINQ solution though

---
+++ Advent2017/Solutions/Day15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2017.Solutions
+{
+    class Day15
+    {
+        public const int A = 16807;
+        public const int B = 48271;
+
+        public IEnumerable<long> GetSequence(long x, long factor, int mod = 1)
+        {
+            while (true)
+            {
+                x = ((x * factor) % 2147483647);
+                if(x % mod == 0)
+                    yield return x;
+            }
+        }
+
+        public int FinalCount(int startA, int startB, int iterations, bool part2 = false)
+        {
+            var a = GetSequence(startA, A, part2 ? 4 : 1);
+            var b = GetSequence(startB, B, part2 ? 8 : 1);
+
+            return a.Zip(b, (seqA, seqB) => (seqA & 0xFFFF) == (seqB & 0xFFFF))
+                .Take(iterations)
+                .Count(val=> val);
+        }
+    }
+}
---
+++ Advent2017/Tests/Day15Fixture.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent2017.Solutions;
+using NUnit.Framework;
+
+namespace Advent2017.Tests
+{
+    [TestFixture]
+    class Day15Fixture
+    {
+        private Day15 _day15 = new Day15();
+
+        [Test]
+        public void GeneratorA()
+        {
+            var expected = new List<long> { 1092455, 1181022009, 245556042, 1744312007, 1352636452 };
+            var result = _day15.GetSequence(65, Day15.A).Take(5).ToList();
+
+            Assert.AreEqual(string.Join(",", expected),
+                            string.Join(",", result));
+        }
+        [Test]
+        public void GeneratorB()
+        {
+            var expected = new List<long> { 430625591, 1233683848, 1431495498, 137874439, 285222916 };
+            var result = _day15.GetSequence(8921, Day15.B).Take(5).ToList();
+
+            Assert.AreEqual(string.Join(",", expected),
+                            string.Join(",", result));
+        }
+
+        [Test]
+        public void Sample()
+        {
+            Assert.AreEqual(1, _day15.FinalCount(65, 8921, 5));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(600, _day15.FinalCount(699, 124, 40000000));
+        }
+
+        [Test]
+        public void GeneratorA_Mod4()
+        {
+            var expected = new List<long> { 1352636452, 1992081072, 530830436, 1980017072, 740335192 };
+            var result = _day15.GetSequence(65, Day15.A, 4).Take(5).ToList();
+
+            Assert.AreEqual(string.Join(",", expected),
+                string.Join(",", result));
+        }
+
+        [Test]
+        public void GeneratorB_Mod8()
+        {
+            var expected = new List<long> { 1233683848, 862516352, 1159784568, 1616057672, 412269392 };
+            var result = _day15.GetSequence(8921, Day15.B, 8).Take(5).ToList();
+
+            Assert.AreEqual(string.Join(",", expected),
+                string.Join(",", result));
+        }
+
+        [Test]
+        public void Sample_Part2_First()
+        {
+            Assert.AreEqual(0, _day15.FinalCount(65, 8921, 1055, true));
+            Assert.AreEqual(1, _day15.FinalCount(65, 8921, 1056, true));
+        }
+
+        [Test]
+        public void Sample_Part2()
+        {
+            Assert.AreEqual(309, _day15.FinalCount(65, 8921, 5000000, true));
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(313, _day15.FinalCount(699, 124, 5000000, true));
+        }
+    }
+}
