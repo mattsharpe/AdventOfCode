520283bd3ba3373a8b4eb3d9efd71713fb446336
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-03 17:32:56 +0000

    Tidied up using statements

--- Advent2017/Solutions/Day4.cs
+++ Advent2017/Solutions/Day4.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Advent2017.Solutions
 {
--- Advent2017/Tests/Day2Fixture.cs
+++ Advent2017/Tests/Day2Fixture.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Advent2017.Solutions;
 using Advent2017.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
