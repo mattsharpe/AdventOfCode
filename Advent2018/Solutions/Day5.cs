84fdd2d300a9bc1e29f11dd3aeddd40f4a2f8539
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-10 20:26:33 +0000

    Day 5 - Collapsing polymer strings

---
+++ Advent2018/Solutions/Day5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Advent2018.Utilities;
+
+namespace Advent2018.Solutions
+{
+    class Day5
+    {
+        public string ProcessPolymerReaction(string input)
+        {
+            //build a stack of processed letters, top of the stack is the next to compare against
+            var stack = new Stack<char>();
+            foreach (var current in input)
+            {
+                var previous = stack.Count > 0 ? stack.Peek() : ' ';
+                
+                if (current <= 'Z' && previous == current + 32 || current >= 'a' && previous == current - 32)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(current);
+                }
+            }
+
+            return string.Join("", stack.Reverse());
+        }
+
+        public int FindShortestPolymer(string polymer)
+        {
+            return Enumerable.Range(65, 24).Select(x =>
+                ProcessPolymerReaction(polymer.Replace(Convert.ToChar(x).ToString(), "", StringComparison.CurrentCultureIgnoreCase)).Length)
+                .Min();
+        }
+    }
+}
---
+++ Advent2018/Tests/Day5Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day5Tests
+    {
+        private Day5 _day5;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day5 = new Day5();
+        }
+
+        [TestMethod]
+        [DataRow("aA","")]
+        [DataRow("abBA", "")]
+        [DataRow("abAB", "abAB")]
+        [DataRow("aabAAB", "aabAAB")]
+        [DataRow("dabAcCaCBAcCcaDA", "dabCBAcaDA")]
+        public void Process(string input, string expected)
+        {
+            Assert.AreEqual(expected, _day5.ProcessPolymerReaction(input));
+        }
+        
+        [TestMethod]
+        public void ProcessPolymerPart1()
+        {
+            var polymer = FileReader.ReadFile("day5.txt").First();
+            Assert.AreEqual(11264, _day5.ProcessPolymerReaction(polymer).Length);
+        }
+
+        [TestMethod]
+        public void PolymerReduction()
+        {
+            var polymer = FileReader.ReadFile("day5.txt").First();
+            Assert.AreEqual(4552, _day5.FindShortestPolymer(polymer));
+        }
+    }
+}
