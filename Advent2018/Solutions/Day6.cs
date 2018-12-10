d205b31d6e481fba5e44c8512d3a699a90c878d5
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-10 23:17:01 +0000

    Day 6 - Calculating Manhattan distnaces between locations in a map to find the closest set.

---
+++ Advent2018/Solutions/Day6.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Advent2018.Utilities;
+
+namespace Advent2018.Solutions
+{
+    public class Day6
+    {
+
+        public List<Location> Parse(string[] input)
+        {
+            return input.Select(x =>
+            {
+                var line = x.Split(",").Select(int.Parse).ToArray();
+                return new Location {X = line[0], Y = line[1]};
+            }).ToList();
+        }
+
+        public int CalculateLargestNonInfiniteArea(string[] input)
+        {
+            var coordinates = Parse(input);
+
+            var minX = coordinates.Min(coords => coords.X);
+            var maxX = coordinates.Max(coords => coords.X);
+
+            var minY = coordinates.Min(coords => coords.Y);
+            var maxY = coordinates.Max(coords => coords.Y);
+            
+            var area = new int[coordinates.Count];
+
+            foreach (var x in Enumerable.Range(minX, maxX - minX + 1))
+            {
+                foreach (var y in Enumerable.Range(minY, maxY - minY + 1))
+                {
+                    var distance = coordinates.Min(coordinate => ManhattanDistance(x, coordinate.X, y, coordinate.Y));
+                    var closestIds = Enumerable.Range(0, coordinates.Count).Where(i => ManhattanDistance(x, coordinates[i].X, y, coordinates[i].Y) == distance).ToArray();
+
+                    if (closestIds.Length != 1)
+                    {
+                        continue;
+                    }
+
+                    if (x == minX || x == maxX || y == minY || y == maxY)
+                    {
+                        closestIds.ForEach(id => area[id] = -1);
+                    }
+                    else
+                    {
+                        closestIds.ForEach(id => area[id]++);
+                    }
+
+                }
+            }
+
+            return area.Max();
+            
+        }
+
+        public int Part2(string[] input)
+        {
+            var coordinates = Parse(input);
+
+            var minX = coordinates.Min(coords => coords.X);
+            var maxX = coordinates.Max(coords => coords.X);
+
+            var minY = coordinates.Min(coords => coords.Y);
+            var maxY = coordinates.Max(coords => coords.Y);
+
+            var result = Enumerable.Range(minX, maxX - minX + 1).Sum(x => 
+                Enumerable.Range(minY, maxY - minX + 1).Select(y => 
+                    coordinates.Select(coord => 
+                        ManhattanDistance(x, coord.X, y, coord.Y)).Sum()).Count(d => d < 10000));
+
+            return result;
+        }
+
+
+        public int ManhattanDistance(int x1, int x2, int y1, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
+    }
+
+    public struct Location
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ Advent2018/Tests/Day6Tests.cs
@@ -0,0 +1,47 @@
+﻿using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day6Tests
+    {
+        private Day6 _day6;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day6 = new Day6();
+        }
+
+        [TestMethod]
+        public void SampleData()
+        {
+            var input = new []{
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9",
+            };
+
+            Assert.AreEqual(17, _day6.CalculateLargestNonInfiniteArea(input));
+        }
+
+        [TestMethod]
+        public void LargestAreaPart1()
+        {
+            var input = FileReader.ReadFile("day6.txt");
+            Assert.AreEqual(4060,  _day6.CalculateLargestNonInfiniteArea(input));
+        }
+
+        [TestMethod]
+        public void SizeOfRegionWithAllLocations()
+        {
+            var input = FileReader.ReadFile("day6.txt");
+            Assert.AreEqual(4060, _day6.Part2(input));
+        }
+    }
+}
