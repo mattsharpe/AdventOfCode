eea6bd089a7764f66a3d521f8e63a84135f696de
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-14 09:17:07 +0000

    Day 7 - Traversing a graph of instructions to find the quickest way to build santas sleigh

---
+++ Advent2018/Solutions/Day7.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Advent2018.Utilities;
+
+namespace Advent2018.Solutions
+{
+    class Day7
+    {
+        private readonly Dictionary<char, Node> _graph = new Dictionary<char, Node>();
+
+        public string CalculatePath(string[] input)
+        {
+            BuildGraph(input);
+
+            var sb = new StringBuilder();
+            
+            while (_graph.Any())
+            {
+                var nextTask = _graph.Values.Where(x => x.Dependencies.Count == 0).OrderBy(x => x.Id).First();
+                _graph.Values.ForEach(x => x.Dependencies.Remove(nextTask));
+                _graph.Remove(nextTask.Id);
+                sb.Append(nextTask.Id);
+            }
+
+            return sb.ToString();
+        }
+
+        public int CalculateTimeTaken(string[] input, int numberOfWorkers = 5, bool waitFor60 = true)
+        {
+            BuildGraph(input);
+            
+            var elapsed = 0;
+
+            var workers = new int[numberOfWorkers];
+            var items = new Node[workers.Length];
+
+            while (_graph.Any() || workers.Any(x=> x > 0)) //if there's any work to do
+            {
+                //if there are any free workers assign them a job
+                for (var i = 0; i < workers.Length && _graph.Values.Any(x => x.Dependencies.Count == 0); i++)
+                {
+                    if (workers[i] != 0) continue;
+                    var nextTask = _graph.Values.Where(x => x.Dependencies.Count == 0).OrderBy(x => x.Id).First();
+                    workers[i] = nextTask.Id - 'A' + 1;
+
+                    //sample data and actual data have different parameters
+                    if (waitFor60)
+                        workers[i] += 60;
+
+                    items[i] = nextTask;
+                    _graph.Remove(items[i].Id);
+                }
+                
+                elapsed++;
+                
+                for (var i = 0; i < workers.Length; i++)
+                {
+                    if (workers[i] == 1)
+                    {
+                        _graph.Values.ForEach(x => x.Dependencies.Remove(items[i]));
+                    }
+                    if (workers[i] >= 1)
+                    {
+                        workers[i]--;
+                    }
+                }
+            }
+
+            return elapsed;
+        }
+
+        private void BuildGraph(string[] input)
+        {
+            foreach (var instruction in input)
+            {
+                var split = instruction.Split(" ");
+                GetOrCreateNode(Convert.ToChar(split[7])).Dependencies.Add(GetOrCreateNode(Convert.ToChar(split[1])));
+            }
+        }
+
+
+        private Node GetOrCreateNode(char id)
+        {
+            if (_graph.ContainsKey(id)) return _graph[id];
+            var node = new Node{Id = id};
+            _graph[id] = node;
+
+            return _graph[id];
+        }
+
+    }
+
+    public class Node   
+    {
+        public char Id { get; set; }
+        public List<Node> Dependencies { get; set; } = new List<Node>();
+    }
+}
---
+++ Advent2018/Tests/Day7Tests.cs
@@ -0,0 +1,60 @@
+﻿using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day7Tests
+    {
+        private Day7 _day7;
+        private readonly string[] _input =
+        {
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin."
+        };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day7 = new Day7();
+        }
+
+        [TestMethod]
+        public void SampleSteps()
+        {
+            
+
+            string result = _day7.CalculatePath(_input);
+            Assert.AreEqual("CABDFE", result);
+        }
+
+        [TestMethod]
+        public void CalculateBuildOrderPart1()
+        {
+            var input = FileReader.ReadFile("day7.txt");
+
+            string result = _day7.CalculatePath(input);
+            Assert.AreEqual("JNOIKSYABEQRUVWXGTZFDMHLPC", result);
+        }
+
+        [TestMethod]
+        public void CalculateTimeTaken()
+        {
+            var result = _day7.CalculateTimeTaken(_input, 2, false);
+            Assert.AreEqual(15, result);
+        }
+        
+        [TestMethod]
+        public void CalculateTimeTakenWithRealData()
+        {
+            var result = _day7.CalculateTimeTaken(FileReader.ReadFile("day7.txt"));
+            Assert.AreEqual(1099, result);
+        }
+    }
+}
