83af44d69379af64e8b17d59a3ab81b829e6e94c
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-14 17:46:03 +0000

    Day 8 Unlocking the navigation system on Santa's sleigh by building a tree and exploring it's nodes

---
+++ Advent2018/Solutions/Day8.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Advent2018.Utilities;
+
+namespace Advent2018.Solutions
+{
+    class Day8
+    {
+        public TreeNode ParseTree(string input)
+        {
+            var nodes = input.Split(" ").Select(int.Parse).ToList();
+            return BuildTree(nodes);
+
+        }
+
+        private TreeNode BuildTree(List<int> nodes)
+        {
+            var enumerator = nodes.GetEnumerator();
+
+            int Next()
+            {
+                enumerator.MoveNext();
+                return enumerator.Current;
+            }
+
+            TreeNode BuildNode()
+            {
+                var node = new TreeNode()
+                {
+                    Nodes = new List<TreeNode>(Next()),
+                    MetaData = new List<int>(Next())
+                };
+
+                for (var i = 0; i < node.Nodes.Capacity; i++)
+                {
+                    node.Nodes.Add(BuildNode());
+                }
+                for (var i = 0; i < node.MetaData.Capacity; i++)
+                {
+                    node.MetaData.Add(Next());
+                }
+                return node;
+            }
+            
+            return BuildNode();
+        }
+        
+
+        public int SumMetaData(TreeNode rootNode)
+        {
+            return rootNode.MetaData.Sum() +
+                   rootNode.Nodes.Sum(SumMetaData);
+        }
+
+        public int ValueOfRootNode(TreeNode node)
+        {
+            if (!node.Nodes.Any())
+            {
+                return node.MetaData.Sum();
+            }
+
+            var res = 0;
+            foreach (var i in node.MetaData)
+            {
+                if (i >= 1 && i <= node.Nodes.Count)
+                {
+                    res += ValueOfRootNode(node.Nodes[i - 1]);
+                }
+            }
+            return res;
+        }
+    }
+
+    public class TreeNode
+    {
+        public List<TreeNode> Nodes { get; set; } = new List<TreeNode>();
+        public List<int> MetaData { get; set; } = new List<int>();
+    }
+
+}
---
+++ Advent2018/Tests/Day8Tests.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day8Tests
+    {
+        private Day8 _day8;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day8 = new Day8();
+        }
+
+        [TestMethod]
+        public void BuildTree()
+        {
+            string input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+            TreeNode tree = _day8.ParseTree(input);
+            ValidateSampleTree(tree);
+        }
+
+        [TestMethod]
+        public void SampleTree()
+        {
+            var tree = new TreeNode
+            {
+                MetaData = {1, 1, 2},
+                Nodes = new List<TreeNode>
+                {
+                    new TreeNode
+                    {
+                        MetaData = {10, 11, 12},
+                    },
+                    new TreeNode
+                    {
+                        MetaData = {2},
+                        Nodes = new List<TreeNode>
+                        {
+                            new TreeNode
+                            {
+                                MetaData = {99}
+                            }
+                        }
+                    },
+                }
+            };
+
+            ValidateSampleTree(tree);
+        }
+
+        private void ValidateSampleTree(TreeNode root)
+        {
+            Assert.AreEqual(2, root.Nodes.Count);
+            CollectionAssert.AreEquivalent(new List<int> { 1, 1, 2 }, root.MetaData);
+            
+            Assert.AreEqual(0, root.Nodes.First().Nodes.Count);
+            CollectionAssert.AreEquivalent(new List<int> { 10,11,12 }, root.Nodes.First().MetaData);
+
+            Assert.AreEqual(1, root.Nodes.Last().Nodes.Count);
+            CollectionAssert.AreEquivalent(new List<int> { 2 }, root.Nodes.Last().MetaData);
+
+            Assert.AreEqual(0, root.Nodes.Last().Nodes.First().Nodes.Count);
+            CollectionAssert.AreEquivalent(new List<int>{ 99 }, root.Nodes.Last().Nodes.First().MetaData);
+        }
+
+        [TestMethod]
+        public void CalculateMetaDataSumOfSampleData()
+        {
+            const string input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+            var tree = _day8.ParseTree(input);
+            Assert.AreEqual(138, _day8.SumMetaData(tree));
+        }
+
+        [TestMethod]
+        public void CalculateMetaDataSum()
+        {
+            var input = FileReader.ReadFile("day8.txt").Single();
+            var tree = _day8.ParseTree(input);
+            Assert.AreEqual(37905, _day8.SumMetaData(tree));
+        }
+
+        [TestMethod]
+        public void ValueOfRootNodeSample()
+        {
+            const string input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+            var tree = _day8.ParseTree(input);
+            Assert.AreEqual(66, _day8.ValueOfRootNode(tree));
+        }
+
+        [TestMethod]
+        public void ValueOfRootNode()
+        {
+            var input = FileReader.ReadFile("day8.txt").Single();
+            var tree = _day8.ParseTree(input);
+            Assert.AreEqual(33891, _day8.ValueOfRootNode(tree));
+        }
+    }
+}
