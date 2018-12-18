ad9166cda57904c8766139f10302a3d2bd956797
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-18 21:32:31 +0000

    Day 9 Part 1 done - playing marbles to find the winning elf.
    Part 2 is going to need a rethink for performance.

---
+++ Advent2018/Solutions/Day9.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent2018.Utilities;
+
+namespace Advent2018.Solutions
+{
+    public class Day9
+    {
+        LinkedListNode<int> FindNextMarble(LinkedListNode<int> node, int amount)
+        {
+            Enumerable.Range(0, amount).ForEach(x => node = node.Next ?? node.List.First);
+            return node;
+        }
+
+        LinkedListNode<int> Previous(LinkedListNode<int> node)
+        {
+            Enumerable.Range(0,7).ForEach(x=> node = node.Previous ?? node.List.Last);
+            return node;
+        }
+
+        public long PlayMarbles(int players, int lastMarble)
+        {
+            var scores = new long[players];
+            var currentPlayer = 0;
+
+            var marbles = new LinkedList<int>();
+            marbles.AddFirst(0);
+            var currentMarble = marbles.First;
+            
+            for (var i = 1; i <= lastMarble; i++)
+            {
+                if (i % 23 == 0)
+                {
+                    scores[currentPlayer] += i;
+                    var sevenBack = Previous(currentMarble);
+                    scores[currentPlayer] += sevenBack.Value;
+                    currentMarble = sevenBack.Next ?? marbles.First;
+                    marbles.Remove(sevenBack);
+                }
+                else
+                {
+                    marbles.AddAfter(FindNextMarble(currentMarble, 1), i);
+                    currentMarble = marbles.Find(i);
+                }
+
+                currentPlayer = (currentPlayer + 1) % players;
+            }
+
+            return scores.Max();
+        }
+    }
+}
---
+++ Advent2018/Tests/Day9Tests.cs
@@ -0,0 +1,36 @@
+﻿using Advent2018.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day9Tests
+    {
+        private readonly Day9 _day9 = new Day9();
+        
+        //9 players; last marble is worth 23 points: high score is 32
+        [TestMethod]
+        public void SampleData()
+        {
+            Assert.AreEqual(32, _day9.PlayMarbles(9, 25));
+        }
+
+        [DataTestMethod]
+        [DataRow(10, 1618, 8317)]
+        [DataRow(13, 7999, 146373)]
+        [DataRow(17, 1104, 2764)]
+        [DataRow(21, 6111, 54718)]
+        [DataRow(30, 5807, 37305)]
+        public void SampleDataSet(int players, int lastMarble, int score)
+        {
+            Assert.AreEqual(score, _day9.PlayMarbles(players, lastMarble));
+        }
+
+        [TestMethod]
+        public void PlayingMarbles()
+        {
+            //416 players; last marble is worth 71617 points
+            Assert.AreEqual(436720, _day9.PlayMarbles(416, 71617));
+        }
+    }
+}
