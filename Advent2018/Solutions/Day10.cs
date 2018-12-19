ef0e0fe0ed0da3ad9ce31d8559960e8a49dd7f28
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-19 12:13:28 +0000

    Day 10 The Stars Align - calculating positions of stars to read a message in the sky

---
+++ Advent2018/Solutions/Day10.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+
+namespace Advent2018.Solutions
+{
+    class Day10
+    {
+        private bool[,] _output;
+        public List<PointOfLight> Lights { get; set; }
+
+        public void Parse(string[] sample)
+        {
+            var regex = new Regex(@"(-\d+|\d+)");
+            //"position=<-3,  6> velocity=< 2, -1>"
+
+            Lights = sample.Select(line =>
+            {
+                var matches = regex.Matches(line).Select(x=> Convert.ToInt32(x.Value)).ToArray();
+                return new PointOfLight
+                {
+                    PositionX = matches[0],
+                    PositionY = matches[1],
+                    VelocityX = matches[2],
+                    VelocityY = matches[3], 
+                };
+            }).ToList();
+        }
+
+        public int MinX => Lights.Min(x => x.PositionX);
+        public int MaxX => Lights.Max(x => x.PositionX);
+
+        public int MinY => Lights.Min(x => x.PositionY);
+        public int MaxY => Lights.Max(x => x.PositionY);
+
+        public int RangeOfX => MaxX + Math.Abs(MinX);
+        public int RangeOfY => MaxY + Math.Abs(MinY);
+
+        //draw the lights current positions
+        public void Draw()
+        {
+            _output = new bool[RangeOfX + 2, RangeOfY + 2];
+
+            foreach (var pointOfLight in Lights)
+            {
+                _output[pointOfLight.PositionX + Math.Abs(MinX), pointOfLight.PositionY + Math.Abs(MinY)] = true;
+            }
+
+            for (var y = 0; y < _output.GetUpperBound(1); y++)
+            {
+                for (var x = 0; x < _output.GetUpperBound(0); x++)
+                {
+                    Console.Write(_output[x, y] ? "#" : ".");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void Update()
+        {
+            foreach (var pointOfLight in Lights)
+            {
+                pointOfLight.PositionX += pointOfLight.VelocityX;
+                pointOfLight.PositionY += pointOfLight.VelocityY;
+            }
+        }
+
+        public bool HasResolved()
+        {      
+            //check that every point of light is adjacent to at least one other
+            return Lights.All(x=> Lights.Any(y=> y.IsAdjacent(x)));
+        }
+
+        public int Solve()
+        {
+            int count = 0;
+            while (!HasResolved())
+            {
+                Update();
+                count++;
+            }
+
+            return count;
+        }
+    }
+
+    public class PointOfLight
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
+
+        public bool IsAdjacent(PointOfLight other)
+        {
+            if (PositionX == other.PositionX - 1 && PositionY == other.PositionY + 1)
+                return true;
+            if (PositionX == other.PositionX && PositionY == other.PositionY + 1)
+                return true;
+            if (PositionX == other.PositionX + 1 && PositionY == other.PositionY + 1)
+                return true;
+            if (PositionX == other.PositionX + 1 && PositionY == other.PositionY)
+                return true;
+            if (PositionX == other.PositionX + 1 && PositionY == other.PositionY - 1)
+                return true;
+            if (PositionX == other.PositionX && PositionY == other.PositionY - 1)
+                return true;
+            if (PositionX == other.PositionX - 1 && PositionY == other.PositionY - 1)
+                return true;
+            if (PositionX == other.PositionX - 1 && PositionY == other.PositionY)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Advent2018/Tests/Day10Tests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        private Day10 _day10;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day10 = new Day10();
+        }
+
+        private string[] _sample = new[]
+        {
+            "position=< 9,  1> velocity=< 0,  2>",
+            "position=< 7,  0> velocity=<-1,  0>",
+            "position=< 3, -2> velocity=<-1,  1>",
+            "position=< 6, 10> velocity=<-2, -1>",
+            "position=< 2, -4> velocity=< 2,  2>",
+            "position=<-6, 10> velocity=< 2, -2>",
+            "position=< 1,  8> velocity=< 1, -1>",
+            "position=< 1,  7> velocity=< 1,  0>",
+            "position=<-3, 11> velocity=< 1, -2>",
+            "position=< 7,  6> velocity=<-1, -1>",
+            "position=<-2,  3> velocity=< 1,  0>",
+            "position=<-4,  3> velocity=< 2,  0>",
+            "position=<10, -3> velocity=<-1,  1>",
+            "position=< 5, 11> velocity=< 1, -2>",
+            "position=< 4,  7> velocity=< 0, -1>",
+            "position=< 8, -2> velocity=< 0,  1>",
+            "position=<15,  0> velocity=<-2,  0>",
+            "position=< 1,  6> velocity=< 1,  0>",
+            "position=< 8,  9> velocity=< 0, -1>",
+            "position=< 3,  3> velocity=<-1,  1>",
+            "position=< 0,  5> velocity=< 0, -1>",
+            "position=<-2,  2> velocity=< 2,  0>",
+            "position=< 5, -2> velocity=< 1,  2>",
+            "position=< 1,  4> velocity=< 2,  1>",
+            "position=<-2,  7> velocity=< 2, -2>",
+            "position=< 3,  6> velocity=<-1, -1>",
+            "position=< 5,  0> velocity=< 1,  0>",
+            "position=<-6,  0> velocity=< 2,  0>",
+            "position=< 5,  9> velocity=< 1, -2>",
+            "position=<14,  7> velocity=<-2,  0>",
+            "position=<-3,  6> velocity=< 2, -1>"
+        };
+
+
+
+        [TestMethod]
+        public void ParseInput()
+        {
+            _day10.Parse(new[] {"position=<-3,  6> velocity=< 2, -1>"});
+            var result = _day10.Lights.First();
+            Assert.AreEqual(-3, result.PositionX);
+            Assert.AreEqual(6, result.PositionY);
+            Assert.AreEqual(2, result.VelocityX);
+            Assert.AreEqual(-1, result.VelocityY);
+
+            _day10.Update();
+
+            var updated = _day10.Lights.First();
+
+            Assert.AreEqual(-1, updated.PositionX);
+            Assert.AreEqual(5, updated.PositionY);
+            Assert.AreEqual(2, updated.VelocityX);
+            Assert.AreEqual(-1, updated.VelocityY);
+        }
+        
+        [TestMethod]
+        public void HasResolved()
+        {
+            _day10.Parse(_sample);
+
+            Assert.IsFalse(_day10.HasResolved());
+
+            Enumerable.Range(0,3).ForEach(x=> _day10.Update());
+            _day10.Draw();
+
+            Assert.IsTrue(_day10.HasResolved());
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            _day10.Parse(FileReader.ReadFile("day10.txt"));
+        }
+
+        [TestMethod]
+        public void SolveSample()
+        {
+            _day10.Parse(_sample);
+            _day10.Solve();
+            _day10.Draw();
+        }
+
+        [TestMethod]
+        public void SolvePart1()
+        {
+            _day10.Parse(FileReader.ReadFile("day10.txt"));
+            _day10.Solve();
+            _day10.Draw();
+        }
+
+        [TestMethod]
+        public void CountStepsSample()
+        {
+            _day10.Parse(_sample);
+            Assert.AreEqual(3,_day10.Solve());
+        }
+
+        [TestMethod]
+        public void CountSteps()
+        {
+            _day10.Parse(FileReader.ReadFile("day10.txt"));
+            Assert.AreEqual(10009,_day10.Solve());
+        }
+
+        [TestMethod]
+        public void HasResolved10009()
+        {
+            _day10.Parse(FileReader.ReadFile("day10.txt"));
+
+            Assert.IsFalse(_day10.HasResolved());
+
+            Enumerable.Range(0, 10009).ForEach(x => _day10.Update());
+            _day10.Draw();
+
+            Assert.IsTrue(_day10.HasResolved());
+        }
+    }
+}
