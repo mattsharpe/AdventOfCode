beefc6cb08e7d2848e2e379ca847ecf866fa60b2
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-20 19:36:58 +0000

    🔋 Calculating the optimum power cell to use based on some slighty questionable mathematics.

---
+++ Advent2018/Solutions/Day11.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Advent2018.Solutions
+{
+    public class PowerCell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Power { get; set; }
+        public int Size { get; set; }
+    }
+
+    class Day11
+    {
+        private readonly int[,] _powerGrid = new int[300, 300];
+
+        public int CalculatePower(int x, int y)
+        {
+            var rackId = x + 10;
+            return (rackId * y + SerialNumber) * rackId / 100 % 10 - 5;
+        }
+
+        public int SerialNumber { get; set; }
+
+        public void PopulateGrid()
+        {
+            Iterate(300, (x, y) =>  _powerGrid[x, y] = CalculatePower(x, y));
+        }
+
+        void Iterate(int range, Action<int, int> action)
+        {
+            for (var x = 0; x < range; x++)
+            {
+                for (var y = 0; y < range; y++)
+                {
+                    action(x, y);
+                }
+            }
+        }
+
+        public PowerCell Solve(int size = 3)
+        {
+            PopulateGrid();
+            var result = new PowerCell();
+            
+            var grid = new int[300, 300];
+            for (var i = 1; i <= size; i++)
+            {
+                //build up the power grid
+                Iterate(300 - i, (x, y) =>
+                {
+                    grid[x, y] += _powerGrid[x + i - 1, y];
+                });
+
+                Iterate(300 - i, (x, y) =>
+                {
+                    var power = 0;
+
+                    for (var j = 0; j < i; j++)
+                    {
+                        power += grid[x, y + j];
+                    }
+
+                    if (power > result.Power)
+                    {
+                        result.Power = power;
+                        result.X = x;
+                        result.Y = y;
+                        result.Size = i;
+                    }
+                });
+            }
+            return result;
+        }
+    }
+}
---
+++ Advent2018/Tests/Day11Tests.cs
@@ -0,0 +1,84 @@
+﻿using Advent2018.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        private Day11 _day11;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day11 = new Day11();
+        }
+
+        [DataTestMethod]
+        [DataRow(3, 5, 8, 4)]
+        [DataRow(122, 79, 57, -5)]
+        [DataRow(217, 196, 39, 0)]
+        [DataRow(101, 153, 71, 4)]
+        public void FuelCellPowerLevels(int x, int y, int serialNumber, int expected)
+        {
+            _day11.SerialNumber = serialNumber;
+            var power = _day11.CalculatePower(x, y);
+            Assert.AreEqual(expected, power);
+        }
+        
+        [TestMethod]
+        public void FindMaxPowerSample()
+        {
+            _day11.SerialNumber = 18;
+            var result = _day11.Solve();
+
+            Assert.AreEqual(29, result.Power);
+            Assert.AreEqual(33, result.X);
+            Assert.AreEqual(45, result.Y);
+        }
+
+        [TestMethod]
+        public void FindMaxPowerSample2()
+        {
+            _day11.SerialNumber = 42;
+            var result = _day11.Solve();
+
+            Assert.AreEqual(30, result.Power);
+            Assert.AreEqual(21, result.X);
+            Assert.AreEqual(61, result.Y);
+        }
+
+        [TestMethod]
+        public void FindMaxPower()
+        {
+            _day11.SerialNumber = 9306;
+            var result = _day11.Solve();
+
+            Assert.AreEqual(30, result.Power);
+            Assert.AreEqual(235, result.X);
+            Assert.AreEqual(38, result.Y);
+        }
+
+        [TestMethod]
+        public void Solve()
+        {
+            _day11.SerialNumber = 18;
+            var result = _day11.Solve();
+            
+            Assert.AreEqual(33, result.X);
+            Assert.AreEqual(45, result.Y);
+            Assert.AreEqual(3, result.Size);
+        }
+
+        [TestMethod]
+        public void SolvePart2()
+        {
+            _day11.SerialNumber = 9306;
+            var result = _day11.Solve(300);
+            
+            Assert.AreEqual(233, result.X);
+            Assert.AreEqual(146, result.Y);
+            Assert.AreEqual(13, result.Size);
+        }
+    }
+}
