62c716c462f650f2b608a444ed48d0203d70af21
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2018-12-21 12:24:22 +0000

    Day 12 - solving generations of plant replication over a time period. Need to convert ints to longs to cope with 50b

---
+++ Advent2018/Solutions/Day12.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Advent2018.Solutions
+{
+    class Day12
+    {
+        public string InitialState { get; set; } = ".#..##..#.....######.....#....####.##.#.#...#...##.#...###..####.##.##.####..######......#..##.##.##";
+        public Dictionary<string,string> Rules = new Dictionary<string, string>();
+
+        public void ParseRules(string[] rules)
+        {
+            Rules = rules.Select(x => x.Split(" ")).ToDictionary(x => x[0], x => x[2]);
+        }
+
+        public PlantGeneration SolveForGenerations(int generations)
+        {
+            var result = new PlantGeneration {PlantPots = InitialState};
+
+            return Enumerable.Range(0, generations).Aggregate(result, (current, generation) => EvolveGeneration(current));
+        }
+
+        public long CountPlantsBasedOnPattern(int generations)
+        {
+            if (generations <= 100)
+                return CountPlants(SolveForGenerations(generations));
+
+            var hundred = CountPlants(SolveForGenerations(100));
+
+            var stableState = CountPlants(SolveForGenerations(101)) - hundred;
+            
+            long result = (generations - 100) * stableState + hundred;
+            
+            return result;
+        }
+
+        public PlantGeneration EvolveGeneration(PlantGeneration generation)
+        {
+            var pots = $"....{generation.PlantPots}....";
+            //foreach pot (where there are at least 2 pots before it or after it)
+            //get the pot sequence i-2, i-1, i, i+1, i+2
+            //lookup the rule 
+            //append the answer to the output
+            //also need to track the offset...
+            var builder = new StringBuilder();
+            for (var i = 2; i < pots.Length-2; i++)
+            {
+                var pot = Enumerable.Range(-2, 5).Aggregate(new StringBuilder(),(sb,x)=> sb.Append(pots[i + x])).ToString();
+                builder.Append(Rules.TryGetValue(pot, out var plant) ? plant : ".");
+            }
+
+            var next = builder.ToString();
+            
+            var result  = new PlantGeneration
+            {
+                Offset = generation.Offset,
+                PlantPots = next
+            };
+            
+            result.Offset -= 2;
+            
+            return result;
+        }
+
+        public int CountPlants(PlantGeneration generation)
+        {
+            return Enumerable.Range(0, generation.PlantPots.Length)
+                .Sum(x => generation.PlantPots[x] == '#' ? x + generation.Offset : 0);
+        }
+    }
+
+    public class PlantGeneration
+    {
+        public int Offset { get; set; }
+        public string PlantPots { get; set; }
+    }
+}
---
+++ Advent2018/Tests/Day12Tests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Linq;
+using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        private Day12 _day12;
+        private string[] SampleRules = new[]
+        {
+            "...## => #",
+            "..#.. => #",
+            ".#... => #",
+            ".#.#. => #",
+            ".#.## => #",
+            ".##.. => #",
+            ".#### => #",
+            "#.#.# => #",
+            "#.### => #",
+            "##.#. => #",
+            "##.## => #",
+            "###.. => #",
+            "###.# => #",
+            "####. => #"
+        };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _day12 = new Day12();
+        }
+
+        [TestMethod]
+        public void PlantPotSample()
+        {
+            _day12.InitialState = "#..#.#..##......###...###";
+
+            _day12.ParseRules(SampleRules);
+
+            Assert.AreEqual("#..#.#..##......###...###", _day12.InitialState);
+
+            var result = _day12.EvolveGeneration(new PlantGeneration{PlantPots = _day12.InitialState});
+
+            Assert.AreEqual("..#...#....#.....#..#..#..#..", result.PlantPots);
+            Console.WriteLine(result);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, "..#...#....#.....#..#..#..#..")]
+        [DataRow(2, "....##..##...##....#..#..#..##...")]
+        [DataRow(3, ".....#.#...#..#.#....#..#..#...#.....")]
+        [DataRow(4, "........#.#..#...#.#...#..#..##..##......")]
+        [DataRow(5, "...........#...##...#.#..#..#...#...#........")]
+        [DataRow(6, ".............##.#.#....#...#..##..##..##.........")]
+        [DataRow(7, "..............#..###.#...##..#...#...#...#...........")]
+        [DataRow(8, "................#....##.#.#.#..##..##..##..##............")]
+        [DataRow(9, "..................##..#..#####....#...#...#...#..............")]
+        [DataRow(10, "...................#.#..#...#.##....##..##..##..##...............")]
+        [DataRow(11, "......................#...##...#.#...#.#...#...#...#.................")]
+        [DataRow(12, "........................##.#.#....#.#...#.#..##..##..##..................")]
+        [DataRow(13, ".........................#..###.#....#.#...#....#...#...#....................")]
+        [DataRow(14, "...........................#....##.#....#.#..##...##..##..##.....................")]
+        [DataRow(15, ".............................##..#..#.#....#....#..#.#...#...#.......................")]
+        [DataRow(16, "..............................#.#..#...#.#...##...#...#.#..##..##........................")]
+        [DataRow(17, ".................................#...##...#.#.#.#...##...#....#...#..........................")]
+        [DataRow(18, "...................................##.#.#....#####.#.#.#...##...##..##...........................")]
+        [DataRow(19, "....................................#..###.#..#.#.#######.#.#.#..#.#...#.............................")]
+        [DataRow(20, "......................................#....##....#####...#######....#.#..##..............................")]
+        public void ProcessGenerations(int generations, string expected)
+        {
+            _day12.InitialState = "#..#.#..##......###...###";
+            _day12.ParseRules(SampleRules);
+            foreach (var i in expected)
+            {
+                var solved = _day12.SolveForGenerations(generations);
+                Assert.AreEqual(expected, solved.PlantPots);
+                Assert.AreEqual(-2 * generations, solved.Offset);
+            }
+        }
+
+        [TestMethod]
+        public void CalculateOffset()
+        {
+            _day12.InitialState = "##..##...##....#..#..#..##";
+            _day12.ParseRules(SampleRules);
+            var result = _day12.SolveForGenerations(1);
+            Assert.AreEqual(".#.#...#..#.#....#..#..#...#..", result.PlantPots);
+            Assert.AreEqual(-2, result.Offset);
+
+        }
+
+        [TestMethod]
+        public void CountPlants()
+        {
+            _day12.InitialState = "#..#.#..##......###...###...........";
+            _day12.ParseRules(SampleRules);
+
+            var generation = _day12.SolveForGenerations(20);
+
+            Assert.AreEqual(325, _day12.CountPlants(generation));
+        }
+
+        [TestMethod]
+        public void Simulate20Generations()
+        {
+            _day12.ParseRules(FileReader.ReadFile("day12.txt"));
+            
+            var result = _day12.SolveForGenerations(20);
+            
+            Assert.AreEqual(3605, _day12.CountPlants(result));
+        }
+
+        [TestMethod]
+        [DataRow(100, 8898)]
+        [DataRow(200, 16998)]
+        [DataRow(300, 25098)]
+        [DataRow(400, 33198)]
+        [DataRow(500, 41298)]
+        public void LookForPattern(int generations, int expected)
+        {
+            var rules = FileReader.ReadFile("day12.txt");
+
+            _day12.ParseRules(rules);
+            Assert.AreEqual(expected, _day12.CountPlants(_day12.SolveForGenerations(generations)));
+            Assert.AreEqual(expected, _day12.CountPlantsBasedOnPattern(generations));
+        }
+
+        [TestMethod]
+        public void FiftyBillionGenerationsOfPlants()
+        {
+            var rules = FileReader.ReadFile("day12.txt");
+            //50 billion is too many to brute force - is there a sequence in the answers?
+
+            _day12.ParseRules(rules);
+            //Assert.AreEqual(123, _day12.CountPlantsBasedOnPattern(500));
+        }
+
+
+
+
+    }
+}
