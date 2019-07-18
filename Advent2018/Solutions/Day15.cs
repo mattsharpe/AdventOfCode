b4cdc7f9979199488cda4cc634cf2850ec1ad58b
Matthew Sharpe <matthew.sharpe@sword-activerisk.com>
2019-07-18 18:57:48 +0100

    Classes for Day15

---
+++ Advent2018/Solutions/Day15.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2018.Solutions
+{
+    class Day15
+    {
+    }
+}
---
+++ Advent2018/Tests/Day15Tests.cs
@@ -0,0 +1,18 @@
+﻿using Advent2018.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day15Tests
+    {
+
+        private Day15 _day15;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _day15 = new Day15();
+        }
+    }
+}
