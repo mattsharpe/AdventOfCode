0ee33267cba8819f832887be02b0b88f68fc78c4
mattsharpe <mattsharpe@users.noreply.github.com>
2019-07-22 20:06:02 +0100

    🧝 vs 👺showdown to protect the hot chocolate recipe for 🎅🏻 (#14)
    * Classes for Day15
    
    * Day 15 Part 1 done - needs some performance tweaking though
    
    * Performance fixes
    
    * Part 2 - looping through elf power to beat the goblins

---
+++ Advent2018/Solutions/Day15.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Advent2018.Solutions
+{
+    class Day15
+    {
+        private char[,] _map;
+        private readonly Dictionary<(int,int),Unit> _units = new Dictionary<(int,int),Unit>();
+        public int Rounds { get; set; }
+        public bool ElvesFtw { get; set; }
+        public int ElfAttackPower { get; set; } = 3;
+
+        public int TotalHitPoints
+        {
+            get { return _units.Sum(x => x.Value.HitPoints); }
+        }
+
+        public int Outcome => Rounds * TotalHitPoints;
+        public bool GameOver { get; set; }
+
+        private readonly List<(int x, int y)> _potentialMoves = new List<(int x, int y)>
+        {
+            (0, -1), (-1, 0), (+1, 0), (0, +1)
+        };
+
+        public bool AreInRange(Unit a, Unit b)
+        {
+            //if there is only once square delta in any direction they're adjacent and in range
+            return Math.Abs(a.Location.x - b.Location.x) + Math.Abs(a.Location.y - b.Location.y) == 1;
+        }
+
+        public void ParseInput(string[] input)
+        {
+            _units.Clear();
+            _map = new char[input[0].Length, input.Length];
+            for (var y = 0; y < input.Length; y++)
+            {
+                var line = input[y];
+                for (var x = 0; x < line.Length; x++)
+                {
+                    _map[x, y] = line[x];
+                    if (line[x] == 'E' || line[x] == 'G')
+                    {
+                        _units.Add((x,y),new Unit
+                        {
+                            Location = (x, y),
+                            Type = line[x] == 'E' ? UnitType.Elf : UnitType.Goblin,
+                            Power = line[x] == 'E' ? ElfAttackPower : 3
+                        });
+                        _map[x, y] = '.';
+                    }
+                }
+            }
+        }
+
+        public void RunGameTurn()
+        {
+            var playOrder = _units.Values.Where(x => x.HitPoints > 0)
+                .OrderBy(x => x.Location.y)
+                .ThenBy(x => x.Location.x);
+
+            foreach (var unit in playOrder.ToList())
+            {
+                ProcessTurnForUnit(unit);
+            }
+        }
+
+        private void ProcessTurnForUnit(Unit unit)
+        {
+            //Console.WriteLine($"Processing turn for {unit} at {unit.Location.x},{unit.Location.y}");
+            if (unit.HitPoints < 1) return;
+            var enemies = _units.Values.Where(x => x.Type != unit.Type && x.HitPoints > 0).ToList();
+            
+            if (!enemies.Any())
+            {
+                GameOver = true;
+            }
+            //are there any enemies in range?
+            var inRange = enemies.Where(enemy => AreInRange(enemy, unit)).ToList();
+
+            if (inRange.Any())
+                ProcessAttack(unit, inRange);
+            else
+            {
+                //Console.WriteLine("Nothing in range - moving to target");
+                var destinations = enemies.SelectMany(x => AdjacentOpenSquares(x.Location)).ToList();
+
+                var moves = destinations.Select(x => ShortestPath(unit.Location, x))
+                    .Where(x => x.Any())
+                    .OrderBy(x => x.Count)
+                    .ThenBy(x=> x.Last().y)
+                    .ThenBy(x=> x.Last().x)
+                    .ToList();
+                if (!moves.Any()) return;
+
+                _units.Remove(unit.Location);
+                unit.Location = moves.First().First();
+                _units.Add(unit.Location, unit);
+
+                inRange = enemies.Where(enemy => AreInRange(enemy, unit)).ToList();
+
+                if (inRange.Any())
+                    ProcessAttack(unit, inRange);
+            }
+        }
+
+        public List<(int x,int y)> ShortestPath((int x, int y) start, (int x, int y) end)
+        {
+            var path = new Dictionary<(int currentX, int currentY), (int previousX, int previousY)>();
+
+            var toExplore = new Queue<(int,int)>();
+            toExplore.Enqueue(start);
+            path.Add(start, (0,0));
+
+            while (toExplore.Any())
+            {
+                var current = toExplore.Dequeue();
+                foreach (var next in AdjacentOpenSquares(current).Where(x => !path.ContainsKey(x)))
+                {
+                    toExplore.Enqueue(next);
+                    path.Add(next, current);
+                }
+            }
+
+            var stack = new Stack<(int,int)>();
+            (int, int) pathStep = end;
+            
+            //if we're blocked and can't reach the target
+            if(!path.ContainsKey(end)) return new List<(int, int)>();
+
+            while (pathStep != start)
+            {
+                stack.Push(pathStep);
+                pathStep = path[pathStep];
+            }
+
+            return stack.ToList();
+        }
+
+        private void ProcessAttack(Unit attacker, List<Unit> enemies)
+        {
+            //Console.WriteLine($"found someone in range of {attacker}");
+            var target = enemies.OrderBy(x => x.HitPoints)
+                .ThenBy(x => x.Location.y)
+                .ThenBy(x => x.Location.x)
+                .First();
+
+            target.HitPoints -= attacker.Power;
+            if (target.HitPoints < 1)
+            {
+                _units.Remove(target.Location);
+                if (target.Type == UnitType.Elf && ElvesFtw) GameOver = true;
+            }
+                
+        }
+
+        /// <summary>
+        /// Returns the coordinates of open squares around this location.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        private List<(int x, int y)> AdjacentOpenSquares((int x, int y) start)
+        {
+            var result = new List<(int x, int y)>();
+            void CheckSquare((int x, int y) location)
+            {
+                if (_map[location.x, location.y] == '.' && !_units.ContainsKey(location))
+                {
+                    result.Add(location);
+                }
+            }
+
+            _potentialMoves.ForEach(transform => CheckSquare((start.x + transform.x, start.y + transform.y)));
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"After {Rounds} rounds");
+            for (var y = 0; y < _map.GetLength(1); y++)
+            {
+                for (var x = 0; x < _map.GetLength(0); x++)
+                {
+                    
+                    if (_units.ContainsKey((x,y)))
+                    {
+                        sb.Append(_units[(x,y)].Type == UnitType.Goblin ? 'G' : 'E');
+                    }
+                    else
+                    {
+                        sb.Append(_map[x, y]);
+                    }
+                }
+
+                sb.AppendLine("   " + string.Join(", ",_units.Values.Where(unit => unit.Location.y == y)
+                    .OrderBy(unit => unit.Location.x)));
+            }
+            return sb.ToString();
+        }
+
+        public void RunGameToCompletion(bool debug = false)
+        {
+            while (!GameOver)
+            {
+                RunGameTurn();
+               
+                if(!GameOver)
+                    Rounds++;
+
+                if(debug)   
+                    Console.WriteLine(ToString());
+            }
+        }
+        
+        public int FindMinAttackPower(string[] input)
+        {
+            ElvesFtw = true;
+            for (var i = 4; ; i++)
+            {
+                ElfAttackPower = i;
+                ParseInput(input);
+                GameOver = false;
+                Rounds = 0;
+                RunGameToCompletion();
+                if (_units.Values.ToList().TrueForAll(x => x.Type == UnitType.Elf))
+                {
+                    this.Rounds = Rounds;
+                    return i;
+                    
+                }
+            }
+            return 0;
+        }
+    }
+
+    class Unit
+    {
+        public int HitPoints { get; set; } = 200;
+        public int Power { get; set; } = 3;
+        public (int x, int y) Location { get; set; }
+        public UnitType Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"{(Type == UnitType.Elf ? "E": "G")}({HitPoints})";
+        }
+    }
+
+    public enum UnitType
+    {
+        Elf,
+        Goblin
+    }
+}
---
+++ Advent2018/Tests/Day15Tests.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.IO;
+using Advent2018.Solutions;
+using Advent2018.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2018.Tests
+{
+    [TestClass]
+    public class Day15Tests
+    {
+
+        private Day15 _day15;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _day15 = new Day15();
+        }
+
+        [TestMethod]
+        public void ShortestPath()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#.G...#",
+                @"#...EG#",
+                @"#.#.#G#",
+                @"#..G#E#",
+                @"#.....#",
+                @"#######"
+            };
+            _day15.ParseInput(input);
+            var path = _day15.ShortestPath((2, 1), (4, 1));
+            Assert.AreEqual(2, path.Count);
+            Assert.AreEqual((3,1), path[0]);
+            Assert.AreEqual((4,1), path[1]);
+        }
+
+        [TestMethod]
+        public void CombatRound()
+        {
+            string[] input =
+                {
+                    @"#######",
+                    @"#.G...#",
+                    @"#...EG#",
+                    @"#.#.#G#",
+                    @"#..G#E#",
+                    @"#.....#",
+                    @"#######"
+                };
+                
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+
+            Assert.AreEqual(47, _day15.Rounds);
+            Assert.AreEqual(590, _day15.TotalHitPoints);
+            Assert.AreEqual(27730, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void SampleData()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#G..#E#",
+                @"#E#E.E#",
+                @"#G.##.#",
+                @"#...#E#",
+                @"#...E.#",
+                @"#######"
+            };
+
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+
+            Assert.AreEqual(37, _day15.Rounds);
+            Assert.AreEqual(982, _day15.TotalHitPoints);
+            Assert.AreEqual(36334, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void SampleData2()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#E..EG#",
+                @"#.#G.E#",
+                @"#E.##E#",
+                @"#G..#.#",
+                @"#..E#.#",
+                @"#######",
+            };
+
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+            Assert.AreEqual(46, _day15.Rounds);
+            Assert.AreEqual(859, _day15.TotalHitPoints);
+            Assert.AreEqual(39514, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void SampleData3()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#E.G#.#",
+                @"#.#G..#",
+                @"#G.#.G#",
+                @"#G..#.#",
+                @"#...E.#",
+                @"#######",
+            };
+
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+            Assert.AreEqual(35, _day15.Rounds);
+            Assert.AreEqual(793, _day15.TotalHitPoints);
+            Assert.AreEqual(27755, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void SampleData4()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#.E...#",
+                @"#.#..G#",
+                @"#.###.#",
+                @"#E#G#G#",
+                @"#...#G#",
+                @"#######",
+            };
+
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+            Assert.AreEqual(54, _day15.Rounds);
+            Assert.AreEqual(536, _day15.TotalHitPoints);
+            Assert.AreEqual(28944, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void SampleData5()
+        {
+            string[] input =
+            {
+                @"#########",
+                @"#G......#",
+                @"#.E.#...#",
+                @"#..##..G#",
+                @"#...##..#",
+                @"#...#...#",
+                @"#.G...G.#",
+                @"#.....G.#",
+                @"#########",
+            };
+
+            _day15.ParseInput(input);
+            _day15.RunGameToCompletion();
+            Assert.AreEqual(20, _day15.Rounds);
+            Assert.AreEqual(937, _day15.TotalHitPoints);
+            Assert.AreEqual(18740, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            _day15.ParseInput(FileReader.ReadFile("day15.txt"));
+
+            _day15.RunGameToCompletion();
+
+            Assert.AreEqual(77, _day15.Rounds);
+            Assert.AreEqual(2944, _day15.TotalHitPoints);
+            Assert.AreEqual(226688, _day15.Outcome);
+        }
+
+        #region Part2
+        [TestMethod]
+        public void Part2SampleData()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#.G...#",
+                @"#...EG#",
+                @"#.#.#G#",
+                @"#..G#E#",
+                @"#.....#",
+                @"#######"
+            };
+            
+            Assert.AreEqual(15, _day15.FindMinAttackPower(input));
+            Assert.AreEqual(29, _day15.Rounds);
+            Assert.AreEqual(172, _day15.TotalHitPoints);
+            Assert.AreEqual(4988, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part2SampleData2()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#E..EG#",
+                @"#.#G.E#",
+                @"#E.##E#",
+                @"#G..#.#",
+                @"#..E#.#",
+                @"#######",
+            };
+            
+            Assert.AreEqual(4, _day15.FindMinAttackPower(input));
+            Assert.AreEqual(33, _day15.Rounds);
+            Assert.AreEqual(948, _day15.TotalHitPoints);
+            Assert.AreEqual(31284, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part2SampleData3()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#E.G#.#",
+                @"#.#G..#",
+                @"#G.#.G#",
+                @"#G..#.#",
+                @"#...E.#",
+                @"#######",
+            };
+            
+            Assert.AreEqual(15, _day15.FindMinAttackPower(input));
+            Assert.AreEqual(37, _day15.Rounds);
+            Assert.AreEqual(94, _day15.TotalHitPoints);
+            Assert.AreEqual(3478, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part2SampleData4()
+        {
+            string[] input =
+            {
+                @"#######",
+                @"#.E...#",
+                @"#.#..G#",
+                @"#.###.#",
+                @"#E#G#G#",
+                @"#...#G#",
+                @"#######",
+            };
+            
+            Assert.AreEqual(12, _day15.FindMinAttackPower(input));
+            Assert.AreEqual(39, _day15.Rounds);
+            Assert.AreEqual(166, _day15.TotalHitPoints);
+            Assert.AreEqual(6474, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part2SampleData5()
+        {
+            string[] input =
+            {
+                @"#########",
+                @"#G......#",
+                @"#.E.#...#",
+                @"#..##..G#",
+                @"#...##..#",
+                @"#...#...#",
+                @"#.G...G.#",
+                @"#.....G.#",
+                @"#########",
+            };
+            
+            Assert.AreEqual(34, _day15.FindMinAttackPower(input));
+            Assert.AreEqual(30, _day15.Rounds);
+            Assert.AreEqual(38, _day15.TotalHitPoints);
+            Assert.AreEqual(1140, _day15.Outcome);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+
+            Assert.AreEqual(34, _day15.FindMinAttackPower(FileReader.ReadFile("day15.txt")));
+            Assert.AreEqual(42, _day15.Rounds);
+            Assert.AreEqual(1499, _day15.TotalHitPoints);
+            Assert.AreEqual(62958, _day15.Outcome);
+        }
+
+
+        #endregion
+    }
+}
