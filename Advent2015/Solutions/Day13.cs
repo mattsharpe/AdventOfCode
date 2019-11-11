8b22b540ea983113b62e734d0e5a1b731af8d569
Matt Sharpe <matt@mattsharpe.co.uk>
2019-11-11 06:05:23 +0000

    Day 13 - finding the optimal seating arrangement for dinner

---
+++ Advent2015/Solutions/Day13.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2015.Solutions
+{
+    class Day13
+    {
+        private Dictionary<string, Dictionary<string, int>> _dinerPreferences;
+
+        public void CalculatePotentialHappiness(string[] sampleData)
+        {
+            BuildAttendeeList(sampleData);
+            GetPermutations(_dinerPreferences.Keys, _dinerPreferences.Count);
+        }
+
+        public void CalculatePotentialHappinessWithMatt(string[] sampleData)
+        {
+            BuildAttendeeList(sampleData);
+            var guests = _dinerPreferences.Keys.ToList();
+
+            foreach (var diner in _dinerPreferences)
+            {
+                diner.Value.Add("Matt", 0);
+            }
+            
+            _dinerPreferences.Add("Matt", guests.ToDictionary(x=>x,y=>0));
+
+            GetPermutations(_dinerPreferences.Keys, _dinerPreferences.Count);
+        }
+
+        public IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+        
+        public int CalculatePreference(List<string> paths)
+        {
+            var score = 0;
+
+            for (var i = 0; i != paths.Count - 1; i++)
+            {
+                score += _dinerPreferences[paths[i]][paths[i + 1]];
+                score += _dinerPreferences[paths[i + 1]][paths[i]];
+            }
+
+            //handle the loop round
+            score += _dinerPreferences[paths.Last()][paths.First()];
+            score += _dinerPreferences[paths.First()][paths.Last()];
+            return score;
+            
+        }
+
+        private void BuildAttendeeList(string[] input)
+        {
+            _dinerPreferences = input.Select(x => x.Split(' ').First())
+                .Distinct().ToDictionary(x=>x, x=> new Dictionary<string, int>());
+
+            foreach (var line in input)
+            {
+                var split = line.Split(' ');
+
+                var diner1 = split[0];
+                var diner2 = split[10].Replace(".", "");
+                var opinion = Convert.ToInt32(split[3]);
+                if (split[2] == "lose")
+                {
+                    opinion *= -1;
+                }
+
+                _dinerPreferences[diner1][diner2] = opinion;
+            }
+        }
+
+        public int FindBestOption()
+        {
+            var options = GetPermutations(_dinerPreferences.Keys, _dinerPreferences.Count);
+            return options.Max(x => CalculatePreference(x.ToList()));
+        }
+    }
+}
---
+++ Advent2015/Tests/Day13Fixture.cs
@@ -0,0 +1,54 @@
+﻿using Advent2015.Solutions;
+using Advent2015.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    [TestClass]
+    public class Day13Fixture
+    {
+        private Day13 _day13;
+
+        private static readonly string[] SampleData = 
+        {
+            "Alice would gain 54 happiness units by sitting next to Bob",
+            "Alice would lose 79 happiness units by sitting next to Carol.",
+            "Alice would lose 2 happiness units by sitting next to David.",
+            "Bob would gain 83 happiness units by sitting next to Alice.",
+            "Bob would lose 7 happiness units by sitting next to Carol.",
+            "Bob would lose 63 happiness units by sitting next to David.",
+            "Carol would lose 62 happiness units by sitting next to Alice.",
+            "Carol would gain 60 happiness units by sitting next to Bob.",
+            "Carol would gain 55 happiness units by sitting next to David.",
+            "David would gain 46 happiness units by sitting next to Alice.",
+            "David would lose 7 happiness units by sitting next to Bob.",
+            "David would gain 41 happiness units by sitting next to Carol."
+        };
+        
+        [TestInitialize]
+        public void Initialize() => _day13 = new Day13();
+        
+        [TestMethod]
+        public void PotentialHappinessSample()
+        {
+            _day13.CalculatePotentialHappiness(SampleData);
+            Assert.AreEqual(330, _day13.FindBestOption());
+        }
+
+        [TestMethod]
+        public void CalculateHappinessForDiners()
+        {
+            var input = FileReader.ReadFile("day13.txt");
+            _day13.CalculatePotentialHappiness(input);
+            Assert.AreEqual(664, _day13.FindBestOption());
+        }
+
+        [TestMethod]
+        public void CalculateHappinessForDinersIfTheyHaveToEatWithMattAsWell()
+        {
+            var input = FileReader.ReadFile("day13.txt");
+            _day13.CalculatePotentialHappinessWithMatt(input);
+            Assert.AreEqual(640, _day13.FindBestOption());
+        }
+    }
+}
