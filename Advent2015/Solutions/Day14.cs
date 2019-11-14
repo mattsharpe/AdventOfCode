ec6efd2e21ce0d975882088d16ae522fd74397c9
Matt Sharpe <matt@mattsharpe.co.uk>
2019-11-14 14:56:33 +0000

    Simulating Reindeer racing

--- Advent2015/Solutions/Day14.cs
+++ Advent2015/Solutions/Day14.cs
@@ -1,9 +1,94 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent2015.Solutions
 {
     class Day14
     {
-        
+        private List<Reindeer> _reindeer;
+
+        public void ParseReindeer(string[] input)
+        {
+            _reindeer = input.Select(x =>
+            {
+                var split = x.Split(' ');
+                return new Reindeer
+                {
+                    Name = split[0],
+                    Speed = Convert.ToInt32(split[3]),
+                    FlightTime = Convert.ToInt32(split[6]),
+                    FlightTimeRemaining = Convert.ToInt32(split[6]),
+                    RestTime = Convert.ToInt32(split[13]),
+                    RestTimeRemaining = Convert.ToInt32(split[13]),
+                };
+            }).ToList();
+        }
+
+        public void Simulate(int time)
+        {
+            foreach (var i in Enumerable.Range(1, time))
+            {
+                foreach (var reindeer in _reindeer)
+                {
+                    reindeer.ProcessTurn();
+                    Console.WriteLine(reindeer.Name + ": " + reindeer.Score);
+                }
+
+                foreach (var reindeer in _reindeer.Where(x => x.DistanceTraveled == _reindeer.Max(y => y.DistanceTraveled)))
+                {
+                    reindeer.Score++;
+                }
+            }
+        }
+
+        public int DistanceWinningReindeerHasTraveled()
+        {
+            return _reindeer.OrderByDescending(x => x.DistanceTraveled).First().DistanceTraveled;
+        }
+
+        public int WinningReindeerScore()
+        {
+            return _reindeer.OrderByDescending(x => x.Score).First().Score;
+        }
+    }
+
+    internal class Reindeer
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int FlightTime { get; set; }
+        public int RestTime { get; set; }
+        public int DistanceTraveled { get; set; }
+        public bool Flying { get; set; } = true;
+
+        public int RestTimeRemaining { get; set; }
+        public int FlightTimeRemaining { get; set; }
+
+        public int Score { get; set; }
+
+        public void ProcessTurn()
+        {
+            if (Flying)
+            {
+                DistanceTraveled += Speed;
+                FlightTimeRemaining--;
+
+                if (FlightTimeRemaining == 0)
+                {
+                    Flying = false;
+                    RestTimeRemaining = RestTime;
+                }
+            }
+            else
+            {
+                RestTimeRemaining--;
+                if (RestTimeRemaining == 0)
+                {
+                    Flying = true;
+                    FlightTimeRemaining = FlightTime;
+                }
+            }
+        }
     }
 }
--- Advent2015/Tests/Day14Fixture.cs
+++ Advent2015/Tests/Day14Fixture.cs
@@ -1,5 +1,4 @@
 using Advent2015.Solutions;
-using Advent2015.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Advent2015.Tests
@@ -12,5 +11,71 @@ namespace Advent2015.Tests
         [TestInitialize]
         public void Initialize() => _day14 = new Day14();
         
+        private readonly string[] _sample = 
+        {
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+        };
+
+        private readonly string[] _input = 
+        {
+            "Rudolph can fly 22 km/s for 8 seconds, but then must rest for 165 seconds.",
+            "Cupid can fly 8 km/s for 17 seconds, but then must rest for 114 seconds.",
+            "Prancer can fly 18 km/s for 6 seconds, but then must rest for 103 seconds.",
+            "Donner can fly 25 km/s for 6 seconds, but then must rest for 145 seconds.",
+            "Dasher can fly 11 km/s for 12 seconds, but then must rest for 125 seconds.",
+            "Comet can fly 21 km/s for 6 seconds, but then must rest for 121 seconds.",
+            "Blitzen can fly 18 km/s for 3 seconds, but then must rest for 50 seconds.",
+            "Vixen can fly 20 km/s for 4 seconds, but then must rest for 75 seconds.",
+            "Dancer can fly 7 km/s for 20 seconds, but then must rest for 119 seconds."
+        };
+
+        [TestMethod]
+        public void SampleDataOneSecond()
+        {
+            _day14.ParseReindeer(_sample);
+            _day14.Simulate(1);
+            Assert.AreEqual(16, _day14.DistanceWinningReindeerHasTraveled());
+        }
+
+        [TestMethod]
+        public void SampleDataTenSeconds()
+        {
+            _day14.ParseReindeer(_sample);
+            _day14.Simulate(10);
+            Assert.AreEqual(160, _day14.DistanceWinningReindeerHasTraveled());
+        }
+
+        [TestMethod]
+        public void SampleDataThousandSeconds()
+        {
+            _day14.ParseReindeer(_sample);
+            _day14.Simulate(1000);
+            Assert.AreEqual(1120, _day14.DistanceWinningReindeerHasTraveled());
+        }
+
+        [TestMethod]
+        public void ReindeerRacePart1()
+        {
+            _day14.ParseReindeer(_input);
+            _day14.Simulate(2503);
+            Assert.AreEqual(2696, _day14.DistanceWinningReindeerHasTraveled());
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            _day14.ParseReindeer(_sample);
+            _day14.Simulate(1000);
+            Assert.AreEqual(689, _day14.WinningReindeerScore());
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            _day14.ParseReindeer(_input);
+            _day14.Simulate(2503);
+            Assert.AreEqual(1084, _day14.WinningReindeerScore());
+        }
     }
 }
