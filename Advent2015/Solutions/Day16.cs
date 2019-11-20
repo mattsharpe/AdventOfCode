be42fe5e5214510b098e0e162ae4cf3d1c12745d
Matt Sharpe <matt@mattsharpe.co.uk>
2019-11-20 10:14:08 +0000

    Identifying the correct Aunt Sue for a thank you card. Filtering a list of partial facts to logically deduce the correct relative.

---
+++ Advent2015/Solutions/Day16.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2015.Solutions
+{
+    class Day16
+    {
+        private static bool Equals(string test, string property, int value)
+        {
+            var regex = new Regex($@"{property}: (\d+)");
+            if (!regex.IsMatch(test)) return true;
+            return Convert.ToInt32(regex.Match(test).Groups[1].Value) == value;
+        }
+
+        private static bool GreaterThan(string test, string property, int value)
+        {
+            var regex = new Regex($@"{property}: (\d+)");
+            if (!regex.IsMatch(test)) return true;
+            return Convert.ToInt32(regex.Match(test).Groups[1].Value) > value;
+        }
+
+        private static bool LessThan(string test, string property, int value)
+        {
+            var regex = new Regex($@"{property}: (\d+)");
+            if (!regex.IsMatch(test)) return true;
+            return Convert.ToInt32(regex.Match(test).Groups[1].Value) < value;
+        }
+
+        public int FilterAuntSue(string[] input, bool part2 = false)
+        {
+            
+            var filters = new List<Func<string, bool>>
+            {
+                x => Equals(x, "children", 3),
+                x => part2 ? GreaterThan(x, "cats", 7) : Equals(x, "cats", 7),
+                x => Equals(x, "samoyeds", 2),
+                x => part2 ? LessThan(x, "pomeranians", 3) : Equals(x, "pomeranians", 3),
+                x => Equals(x, "akitas", 0),
+                x => Equals(x, "vizslas", 0),
+                x => part2 ? LessThan(x, "goldfish", 5) : Equals(x, "goldfish", 5),
+                x => part2 ? GreaterThan(x, "trees", 3) : Equals(x, "trees", 3),
+                x => Equals(x, "cars", 2),
+                x => Equals(x, "perfumes", 1),
+            };
+
+            var filtered = ApplyFilters(input, filters);
+
+            return Convert.ToInt32(filtered.Single().Split(' ')[1].Replace(":", ""));
+
+        }
+
+        public string[] ApplyFilters(string[] input,  List<Func<string, bool>> filters)
+        {
+            foreach (var filter in filters)
+            {
+                input = input.Where(filter).ToArray();
+            }
+
+            return input;
+        }
+    }
+
+}
---
+++ Advent2015/Tests/Day16Fixture.cs
@@ -0,0 +1,30 @@
+﻿using Advent2015.Solutions;
+using Advent2015.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    [TestClass]
+    public class Day16Fixture
+    {
+        private Day16 _day16;
+
+        [TestInitialize] 
+        public void Initialize() => _day16 = new Day16();
+
+        [TestMethod]
+        public void FindAuntSue()
+        {
+            var sueId = _day16.FilterAuntSue(FileReader.ReadFile("day16.txt"));
+            Assert.AreEqual(373, sueId);
+        }
+
+        [TestMethod]
+        public void FindAuntSuePart2()
+        {
+            var sueId = _day16.FilterAuntSue(FileReader.ReadFile("day16.txt"), true);
+            Assert.AreEqual(260, sueId);
+        }
+
+    }
+}
