e088c027973b22987fa17ed1d39026e9d4940868
mattsharpe <mattsharpe@users.noreply.github.com>
2019-11-21 14:30:31 +0000

    Feature/2016 day16 (#25)
    * Identifying the correct Aunt Sue for a thank you card. Filtering a list of partial facts to logically deduce the correct relative.
    
    * Day 17, Pouring out the EggNog into containers (Bin packing problem)

---
+++ Advent2015/Solutions/Day17.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Advent2015.Solutions
+{
+    class Day17
+    {
+        public int FillContainers(int[] containers, int amount)
+        {
+            return Count(amount, containers.Length, null, containers);
+        }
+
+        public int Count(int total, int n, int? i, int[] containers)
+        {
+            i = i ?? 0;
+
+            if (n < 0) {
+                return 0;
+            } if (total == 0) {
+                return 1;
+            } if (i == containers.Length || total < 0) {
+                return 0;
+            }
+
+            var head = Count(total, n, i + 1, containers);
+            var tail = Count(total - containers[i.Value], n - 1, i + 1, containers);
+            
+            return head + tail; 
+        }
+
+        public int MinimumNumberOfContainers(int[] containers, int amount)
+        {
+            var i = 1;
+            var result = 0;
+            while (result==0) {
+                result = Count(amount, i++, null, containers);
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Advent2015/Tests/Day17Fixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    [TestClass]
+    public class Day17Fixture
+    {
+        private Day17 _day17;
+
+        [TestInitialize]
+        public void Initialize() => _day17 = new Day17();
+
+        [TestMethod]
+        public void EggNogBatchingSample()
+        {
+            var containers = new[] { 20, 15, 10, 5, 5 };
+            var amount = 25;
+            Assert.AreEqual(4, _day17.FillContainers(containers, amount));
+        }
+
+        [TestMethod]
+        public void EggNogBatchingPart1()
+        {
+            var containers = FileReader.ReadFile("day17.txt").Select(x=>Convert.ToInt32(x)).ToArray();
+            var amount = 150;
+            
+            Assert.AreEqual(654, _day17.FillContainers(containers, amount));
+        }
+
+        [TestMethod]
+        public void MinimumNumberOfContainersSample()
+        {
+            var containers = new[] { 20, 15, 10, 5, 5 };
+            var amount = 25;
+            
+            Assert.AreEqual(3, _day17.MinimumNumberOfContainers(containers, amount));
+        }
+
+        [TestMethod]
+        public void MinimumNumberOfContainersPart2()
+        {
+            var containers = FileReader.ReadFile("day17.txt").Select(x=>Convert.ToInt32(x)).ToArray();
+            var amount = 150;
+            
+            Assert.AreEqual(57, _day17.MinimumNumberOfContainers(containers, amount));
+        }
+    }
+}
