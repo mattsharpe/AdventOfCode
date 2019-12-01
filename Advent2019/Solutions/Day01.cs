0d8f14a78fbc8c0f00b72f5a5a7f86c6c6b6e419
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-01 16:15:53 +0000

    2019 Day 1

---
+++ Advent2019/Solutions/Day01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day01
+    {
+        public int CalculateFuel(int mass)
+        {
+            return mass / 3 - 2;
+        }
+
+        public int TotalFuelRequirement(string[] input)
+        {
+            return input.Sum(x => CalculateFuel(Convert.ToInt32(x)));
+        }
+    }
+}
---
+++ Advent2019/Tests/Day01Tests.cs
@@ -0,0 +1,32 @@
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        private Day01 _day1;
+
+        [TestInitialize] 
+        public void Initialize() => _day1 = new Day01();
+
+        [TestMethod]
+        public void TotalFuelRequirement()
+        {
+            var input = FileReader.ReadFile("day01.txt");
+            Assert.AreEqual(3372695,_day1.TotalFuelRequirement(input));
+        }
+
+        [DataTestMethod]
+        [DataRow(12, 2)]
+        [DataRow(14, 2)]
+        [DataRow(1969, 654)]
+        [DataRow(100756, 33583)]
+        public void FuelForModule(int mass, int expected)
+        {
+            Assert.AreEqual(expected, _day1.CalculateFuel(mass));
+        }
+    }
+}
