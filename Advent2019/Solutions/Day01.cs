41aee0743d4610d37ddb6a112c37c3051026f711
mattsharpe <mattsharpe@users.noreply.github.com>
2019-12-02 08:40:59 +0000

    2019 Day 1 (#27)
    Calculating how much fuel we need for 🎅 in his 🚀

---
+++ Advent2019/Solutions/Day01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day01
+    {
+        public int CalculateFuel(int mass)
+        {
+            return mass / 3 - 2;
+        }
+
+        public int TotalFuelRequirement(string[] input)
+        {
+            return input.Sum(x => CalculateFuel(Convert.ToInt32(x)));
+        }
+
+        public int CalculateFuelWithExtra(int mass)
+        {
+            var result = 0;
+            while (CalculateFuel(mass) > 0)
+            {
+                var next = CalculateFuel(mass);
+                result += next;
+                mass = next;
+            }
+
+            return result;
+        }
+
+        
+        public int TotalFuelRequirementWithExtra(string[] input)
+        {
+            return input.Sum(x => CalculateFuelWithExtra(Convert.ToInt32(x)));
+        }
+    }
+}
---
+++ Advent2019/Utilities/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Advent2019.Utilities
+{
+    public class FileReader
+    {
+        public static string[] ReadFile(string filename)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var stream = asm.GetManifestResourceStream($"Advent2019.TestData.{filename}");
+
+            if (stream == null)
+                throw new Exception($"Could not load file {filename}");
+
+            var results = new List<string>();
+            using (var reader = new StreamReader(stream))
+            {
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    results.Add(line);
+            }
+            return results.ToArray();
+        }
+    }
+}
