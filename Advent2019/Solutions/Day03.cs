1e0c29ce7fc94d5434b8fc464c78fe9f22e77341
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-03 12:13:56 +0000

    Crossed Wire Intersection, finding the closest place that 2 wires intesect based on their paths

---
+++ Advent2019/Solutions/Day03.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day03
+    {
+        
+        private readonly Dictionary<string, (int x, int y)> _directions = new Dictionary<string, (int x, int y)>
+        {
+            {"U", (0, 1) },
+            {"D", (0, -1) },
+            {"L", (-1, 0) },
+            {"R", (1, 0) }
+        };
+
+        public int FindMostCentralIntersection(string first, string second)
+        {
+            var path = GetVisited(first.Split(","));
+            path.IntersectWith(GetVisited(second.Split(",")));
+            path.Remove((0, 0));
+            return path.Min(loc => ManhattanDistance(0, loc.x, 0, loc.y));
+        }
+
+        public int ManhattanDistance(int x1, int x2, int y1, int y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
+        public HashSet<(int x, int y)> GetVisited(string[] instructions)
+        {
+            var result = new HashSet<(int x, int y)>();
+            (int x, int y) currentLocation = (0, 0);
+            
+            result.Add(currentLocation);
+
+            foreach (var instruction in instructions)
+            {
+                var direction = instruction.Substring(0, 1);
+                var distance = Convert.ToInt32(instruction.Substring(1, instruction.Length-1));
+                var (x, y) = _directions[direction];
+                
+                for (var i = 0; i < distance; i++)
+                {
+                    currentLocation.x += x;
+                    currentLocation.y += y;
+                    result.Add(currentLocation);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Advent2019/Tests/Day03Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day03Tests
+    {
+        private Day03 _day3;
+
+        [TestInitialize] 
+        public void Initialize() => _day3 = new Day03();
+
+        [TestMethod]
+        public void BuildVisitedSet()
+        {
+            var result = _day3.GetVisited(new [] {"R8","U5","L5","D3"});
+            foreach (var valueTuple in result)
+            {
+                Console.WriteLine($"{valueTuple.x}, {valueTuple.y}");
+            }
+
+        }
+
+        [DataTestMethod]
+        [DataRow("R8,U5,L5,D3", "U7,R6,D4,L4", 6)]
+        [DataRow("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [DataRow("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void CrossedWireIntersection(string first, string second, int expected)
+        {
+            Assert.AreEqual(expected, _day3.FindMostCentralIntersection(first,second));
+        }
+
+        [TestMethod]
+        public void CrossedWireIntersectionPart1()
+        {
+            var input = FileReader.ReadFile("day03.txt");
+            Assert.AreEqual(1983, _day3.FindMostCentralIntersection(input.First(), input.Last()));
+        }
+
+    }
+}
