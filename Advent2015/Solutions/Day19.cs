9e079c31fed9723323d4b378914a57029b90acac
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-03 09:39:51 +0000

    Start of 2015 Day 19

---
+++ Advent2015/Solutions/Day19.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Advent2015.Solutions
+{
+    class Day19
+    {
+        Dict
+        public HashSet<string> GenerateReplacements(string input)
+        {
+
+            return new HashSet<string>();
+        }
+
+        public void ParseSubstitutionRules(string[] rules)
+        {
+
+        }
+    }
+}
---
+++ Advent2015/Tests/Day19Fixture.cs
@@ -0,0 +1,30 @@
+﻿using Advent2015.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    [TestClass]
+    public class Day19Fixture
+    {
+        private Day19 _day19;
+
+        [TestInitialize]
+        public void Initialize() => _day19 = new Day19();
+
+        [TestMethod]
+        public void HoHoho()
+        {
+            string[] rules =
+            {
+                "H => HO",
+                "H => OH",
+                "O => HH"
+            };
+            var input = "HOH";
+
+            _day19.ParseSubstitutionRules(rules);
+            _day19.GenerateReplacements(input);
+        }
+
+    }
+}
