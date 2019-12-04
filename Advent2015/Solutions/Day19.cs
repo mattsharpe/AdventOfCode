ad12e0b196d810355933f0723a10357bb6da2c6e
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-04 13:48:56 +0000

    Chemistry is Easy - Generating molecule chains from replacements. Part1 is a simple regex replacement. Part 2 looks like it will nastier...

--- Advent2015/Solutions/Day19.cs
+++ Advent2015/Solutions/Day19.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Advent2015.Solutions
 {
     class Day19
     {
-        Dict
+        private List<(string key, string replacement)> _substitutions = new List<(string key, string replacement)>();
+
         public HashSet<string> GenerateReplacements(string input)
         {
+            HashSet<string> options = new HashSet<string>();
 
-            return new HashSet<string>();
+            foreach (var substitution in _substitutions)
+            {
+                var matches = Regex.Matches(input, substitution.key); //RegexOptions Compiled
+                foreach (Match  match in matches)
+                {
+                    options.Add(input.Remove(match.Index, match.Length)
+                        .Insert(match.Index, substitution.replacement));
+                }
+            }
+
+            return options;
         }
 
         public void ParseSubstitutionRules(string[] rules)
         {
-
+            _substitutions = rules.Select(x =>
+            {
+                var match = Regex.Match(x, @"(\w+)\s=\>\s(\w+)");
+                return (match.Groups[1].Value, match.Groups[2].Value);
+            }).ToList();
         }
     }
 }
--- Advent2015/Tests/Day19Fixture.cs
+++ Advent2015/Tests/Day19Fixture.cs
@@ -1,4 +1,7 @@
-﻿using Advent2015.Solutions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advent2015.Solutions;
+using Advent2015.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Advent2015.Tests
@@ -12,7 +15,7 @@ namespace Advent2015.Tests
         public void Initialize() => _day19 = new Day19();
 
         [TestMethod]
-        public void HoHoho()
+        public void ParseSubstitutionRules()
         {
             string[] rules =
             {
@@ -23,7 +26,36 @@ namespace Advent2015.Tests
             var input = "HOH";
 
             _day19.ParseSubstitutionRules(rules);
-            _day19.GenerateReplacements(input);
+            var results = _day19.GenerateReplacements(input);
+            Assert.AreEqual(4, results.Count);
+        }
+        [TestMethod]
+        public void ParseSubstitutionRulesHoHoHo()
+        {
+            string[] rules =
+            {
+                "H => HO",
+                "H => OH",
+                "O => HH"
+            };
+            var input = "HOHOHO";
+
+            _day19.ParseSubstitutionRules(rules);
+            var results = _day19.GenerateReplacements(input);
+            Assert.AreEqual(7, results.Count);
+        }
+
+
+        [TestMethod]
+        public void GenerateSubstitutions()
+        {
+            var puzzleInput = FileReader.ReadFile("day19.txt");
+            string[] rules = new List<string>(puzzleInput).Where(x => x.Contains("=>")).ToArray();
+            var input = puzzleInput.Last();
+
+            _day19.ParseSubstitutionRules(rules);
+            var results = _day19.GenerateReplacements(input);
+            Assert.AreEqual(510, results.Count);
         }
 
     }
