8d262a8f3f119e0e59c74ac17826b9a8095db94d
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-05 23:45:15 +0000

    Extended the intcode computer to handle value and indirect addresses as well as input / output

---
+++ Advent2019/Tests/Day05Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day05Tests
+    {
+        private IntCodeComputer _computer;
+
+        [TestInitialize]
+        public void Initialize() => _computer = new IntCodeComputer();
+
+        [TestMethod]
+        public void DiagnosticOutput()
+        {
+            var input = FileReader.ReadFile("day05.txt");
+            _computer.InitializePositions(input.First());
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(13547311, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var input = 1002;
+            Console.WriteLine(input % 1000);
+            Console.WriteLine(input.ToString("D5"));
+        }
+
+        [TestMethod]
+        public void PassThroughInput()
+        {
+            _computer.InitializePositions("3,0,4,0,99");
+            _computer.Inputs.Enqueue(123);
+            _computer.RunProgram();
+            Assert.AreEqual(123, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void SampleProgram()
+        {
+            _computer.InitializePositions("1002,4,3,4,33");
+            _computer.RunProgram();
+            Assert.AreEqual(99, _computer.Addresses[4]);
+        }
+
+        [TestMethod]
+        public void NegativeNumbersAreHandledCorrectly()
+        {
+            _computer.InitializePositions("1101,100,-1,4,0");
+            _computer.RunProgram();
+            Assert.AreEqual(99, _computer.Addresses[4]);
+        }
+    }
+}
