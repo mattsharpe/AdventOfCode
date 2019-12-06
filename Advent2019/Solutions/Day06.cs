d23dc7bca34ae5ea31f78d3de7ff257f0f8c0114
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-06 10:39:23 +0000

    Calulating total orbit paths to rescue Santa, using a directed graph modelled as a dictionary

---
+++ Advent2019/Solutions/Day06.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day06
+    {
+        private Dictionary<string, string> _graph;
+        
+        public void BuildGraph(string[] input)
+        {
+            _graph = input
+                .Select(relationship => relationship.Split(")"))
+                .ToDictionary(split => split[1], split => split[0]);
+        }
+
+        public int Orbits()
+        {
+            return _graph.Keys.Sum(x => GetParents(x).Count());
+        }
+
+        public IEnumerable<string> GetParents(string node)
+        {
+            while (node != null)
+            {
+                node = _graph.GetValueOrDefault(node, null);
+                if (node != null)
+                {
+                    yield return node;
+                }
+            }
+        }
+    }
+}
---
+++ Advent2019/Tests/Day06Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day06Tests
+    {
+        private Day06 _day6;
+        private string[] _sampleInput = new[]
+        {
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+        };
+
+        [TestInitialize]
+        public void Initialize() => _day6 = new Day06();
+
+        [DataTestMethod]
+        [DataRow("D", 3)]
+        [DataRow("L", 7)]
+        [DataRow("COM", 0)]
+        public void GetPathLength(string node, int length)
+        {
+            _day6.BuildGraph(_sampleInput);
+            var parents = _day6.GetParents(node);
+            Console.WriteLine(string.Join(",",parents));
+            Assert.AreEqual(length, parents.Count());
+            
+        }
+
+        [TestMethod]
+        public void TotalOrbitsSample()
+        {
+            _day6.BuildGraph(_sampleInput);
+            Assert.AreEqual(42, _day6.Orbits());
+        }
+
+        [TestMethod]
+        public void TotalOrbits()
+        {
+            _day6.BuildGraph(FileReader.ReadFile("day06.txt"));
+            Assert.AreEqual(314702, _day6.Orbits());
+        }
+    }
+}
