fbd5497012e00038ff07c82441d792b2a854d6e5
mattsharpe <mattsharpe@users.noreply.github.com>
2019-12-06 00:13:10 +0000

    Adding the IntCodeComputer with a variety of instructions
    * Extended the intcode computer to handle value and indirect addresses as well as input / output
    
    * Extending the intcode computer to include additional instructions

---
+++ Advent2019/Solutions/IntcodeComputer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class IntCodeComputer
+    {
+        public int[] Addresses { get; private set; }
+        public Queue<int> Inputs { get; } = new Queue<int>();
+        public List<int> Outputs { get; } = new List<int>();
+
+        public void RunProgram()
+        {
+            for (var instructionPointer = 0; instructionPointer < Addresses.Length;)
+            {
+                var instruction = Addresses[instructionPointer];
+                var opCode = (OpCode)(instruction % 100);
+                bool immediate1 = (instruction / 100) % 10  > 0;
+                bool immediate2 = (instruction / 1000) % 10 > 0;
+
+                switch (opCode)
+                {
+                    case OpCode.Add:
+                        var first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        var second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        Addresses[Addresses[instructionPointer + 3]] = first + second;
+                        instructionPointer += 4;
+                        break;
+                    case OpCode.Multiply: 
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        Addresses[Addresses[instructionPointer + 3]] = first * second;
+                        instructionPointer += 4;
+                        break;
+                    case OpCode.Input:
+                        Addresses[Addresses[instructionPointer + 1]] = Inputs.Dequeue();
+                        instructionPointer += 2;
+                        break;
+                    case OpCode.Output:
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        Outputs.Add(first);
+                        instructionPointer += 2;
+                        break;
+                    case OpCode.JumpIfTrue:
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        instructionPointer = first != 0 ? second : instructionPointer + 3;
+                        break;
+                    case OpCode.JumpIfFalse:
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        instructionPointer = first == 0 ? second : instructionPointer + 3;
+                        break;
+                    case OpCode.LessThan:
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        Addresses[Addresses[instructionPointer + 3]] = first < second ? 1 : 0;
+                        instructionPointer += 4;
+                        break;
+                    case OpCode.Equals:
+                        first = immediate1 ? Addresses[instructionPointer + 1] : Addresses[Addresses[instructionPointer + 1]];
+                        second = immediate2 ? Addresses[instructionPointer + 2] : Addresses[Addresses[instructionPointer + 2]];
+                        Addresses[Addresses[instructionPointer + 3]] = first == second ? 1 : 0;
+                        instructionPointer += 4;
+                        break;
+
+                    case OpCode.Halt:
+                        return;
+                    default:
+                        throw new Exception($"Unrecognised Op Code {opCode}");
+                }
+            }
+        }
+
+        public void InitializePositions(string program)
+        {
+            Addresses = program.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+        }
+
+
+    }
+    
+    enum OpCode
+    {
+        Add = 1, 
+        Multiply= 2,
+        Input = 3,
+        Output= 4, 
+        JumpIfTrue = 5,
+        JumpIfFalse = 6,
+        LessThan = 7,
+        Equals = 8,
+        Halt = 99
+    }
+}
---
+++ Advent2019/Tests/Day05Tests.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day05Tests
+    {
+        private IntCodeComputer _computer;
+
+        [TestInitialize]
+        public void Initialize() => _computer = new IntCodeComputer();
+
+        [TestMethod]
+        public void DiagnosticOutput()
+        {
+            var input = FileReader.ReadFile("day05.txt");
+            _computer.InitializePositions(input.First());
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(13547311, _computer.Outputs.Last());
+        }
+        
+        [TestMethod]
+        public void PassThroughInput()
+        {
+            _computer.InitializePositions("3,0,4,0,99");
+            _computer.Inputs.Enqueue(123);
+            _computer.RunProgram();
+            Assert.AreEqual(123, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void SampleProgram()
+        {
+            _computer.InitializePositions("1002,4,3,4,33");
+            _computer.RunProgram();
+            Assert.AreEqual(99, _computer.Addresses[4]);
+        }
+
+        [TestMethod]
+        public void NegativeNumbersAreHandledCorrectly()
+        {
+            _computer.InitializePositions("1101,100,-1,4,0");
+            _computer.RunProgram();
+            Assert.AreEqual(99, _computer.Addresses[4]);
+        }
+
+        [TestMethod]
+        public void EqualToEight()
+        {
+            _computer.InitializePositions("3,9,8,9,10,9,4,9,99,-1,8");
+            _computer.Inputs.Enqueue(8);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void EqualToEight_Immediate()
+        {
+            _computer.InitializePositions("3,3,1108,-1,8,3,4,3,99");
+            _computer.Inputs.Enqueue(8);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void NotEqualToEight_Immediate()
+        {
+            _computer.InitializePositions("3,3,1108,-1,8,3,4,3,99");
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void NotEqualToEight()
+        {
+            _computer.InitializePositions("3,9,8,9,10,9,4,9,99,-1,8");
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void LessThanEight()
+        {
+            _computer.InitializePositions("3,9,7,9,10,9,4,9,99,-1,8");
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void NotLessThanEight()
+        {
+            _computer.InitializePositions("3,9,7,9,10,9,4,9,99,-1,8");
+            _computer.Inputs.Enqueue(9);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+        
+        [TestMethod]
+        public void LessThanEight_Immediate()
+        {
+            _computer.InitializePositions("3,3,1107,-1,8,3,4,3,99");
+            _computer.Inputs.Enqueue(1);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void NotLessThanEight_Immediate()
+        {
+            _computer.InitializePositions("3,3,1107,-1,8,3,4,3,99");
+            _computer.Inputs.Enqueue(9);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void JumpImmediateZero()
+        {
+            _computer.InitializePositions("3,3,1105,-1,9,1101,0,0,12,4,12,99,1");
+            _computer.Inputs.Enqueue(0);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void JumpImmediateNonZero()
+        {
+            _computer.InitializePositions("3,3,1105,-1,9,1101,0,0,12,4,12,99,1");
+            _computer.Inputs.Enqueue(123);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void JumpPositionZero()
+        {
+            _computer.InitializePositions("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
+            _computer.Inputs.Enqueue(0);
+            _computer.RunProgram();
+            Assert.AreEqual(0, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void JumpPositionNonZero()
+        {
+            _computer.InitializePositions("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9");
+            _computer.Inputs.Enqueue(123);
+            _computer.RunProgram();
+            Assert.AreEqual(1, _computer.Outputs.Last());
+        }
+
+        [DataTestMethod]
+        [DataRow(7, 999)]
+        [DataRow(8, 1000)]
+        [DataRow(9, 1001)]
+        public void SampleProgramme(int input, int expected)
+        {
+            _computer.InitializePositions("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
+            _computer.Inputs.Enqueue(input);
+            _computer.RunProgram();
+            Assert.AreEqual(expected, _computer.Outputs.Last());
+        }
+
+        [TestMethod]
+        public void DiagnosticTest()
+        {
+            var input = FileReader.ReadFile("day05.txt");
+            _computer.InitializePositions(input.First());
+            _computer.Inputs.Enqueue(5);
+            _computer.RunProgram();
+            Assert.AreEqual(236453, _computer.Outputs.Last());
+        }
+    }
+}
