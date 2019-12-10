4aac8e37418da85bd5c9f4d6688d0652b81e78b0
mattsharpe <mattsharpe@users.noreply.github.com>
2019-12-10 12:24:04 +0000

    Flatting pixels of an image to decode a hidden BIOS password (#35)
    * Initial Day 8
    
    * Printing out the flattened layers of the image to decode the message

---
+++ Advent2019/Tests/Day08Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day08Tests
+    {
+        private Day08 _day08;
+
+        [TestInitialize]
+        public void TestInitialize() => _day08 = new Day08();
+
+        [TestMethod]
+        public void ParseSmallInput()
+        {
+            var input = "123456789012";
+            _day08.ParseLayers(input,3,2);
+        }
+
+        [TestMethod]
+        public void ParseInput()
+        {
+            var input = FileReader.ReadFile("day08.txt").First();
+            _day08.ParseLayers(input, 25, 6);
+            Assert.AreEqual(1463, _day08.Checksum());
+        }
+
+        [TestMethod]
+        public void PrintImage()
+        {
+            var input = FileReader.ReadFile("day08.txt").First();
+            _day08.ParseLayers(input, 25, 6);
+            _day08.FlattenLayers();
+        }
+    }
+}
