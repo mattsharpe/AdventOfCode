23791acb61c6fbd3d64989f44467bfd4e916112d
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-10 12:05:22 +0000

    Printing out the flattened layers of the image to decode the message

--- Advent2019/Solutions/Day08.cs
+++ Advent2019/Solutions/Day08.cs
@@ -1,10 +1,43 @@
-﻿namespace Advent2019.Solutions
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Advent2019.Solutions
 {
     class Day08
     {
+        private List<string> _layers = new List<string>();
+        
         public void ParseLayers(string input, int width, int height)
         {
+            var numberOfLayers = input.Length / (width * height);
+            for (var i = 0; i < numberOfLayers; i++)
+            {
+                _layers.Add(new string(input.Skip(i * width * height).Take(width * height).ToArray()));
+            }
+        }
 
+        public int Checksum()
+        {
+            var ordered = _layers.OrderBy(x=>x.Count(y=>y.Equals('0')));
+            var layer = ordered.First();
+            return layer.Count(x => x.Equals('1')) * layer.Count(x => x.Equals('2'));
+        }
+        
+        public void FlattenLayers()
+        {
+            var flattenedImage = new StringBuilder();
+            for (var i = 0; i < _layers[0].Length; i++)
+            {
+                flattenedImage.Append(_layers.Select(x=>x[i]).First(x=> x != '2') == '1' ? '#' : '.');
+                if(i%25 == 24)
+                {
+                    flattenedImage.AppendLine("");
+                }
+            }
+            Console.WriteLine(flattenedImage);
         }
     }
 }
--- Advent2019/Tests/Day08Tests.cs
+++ Advent2019/Tests/Day08Tests.cs
@@ -1,10 +1,12 @@
-﻿using Advent2019.Solutions;
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Advent2019.Tests
 {
     [TestClass]
-    class Day08Tests
+    public class Day08Tests
     {
         private Day08 _day08;
 
@@ -16,6 +18,22 @@ namespace Advent2019.Tests
         {
             var input = "123456789012";
             _day08.ParseLayers(input,3,2);
+        }
+
+        [TestMethod]
+        public void ParseInput()
+        {
+            var input = FileReader.ReadFile("day08.txt").First();
+            _day08.ParseLayers(input, 25, 6);
+            Assert.AreEqual(1463, _day08.Checksum());
+        }
+
+        [TestMethod]
+        public void PrintImage()
+        {
+            var input = FileReader.ReadFile("day08.txt").First();
+            _day08.ParseLayers(input, 25, 6);
+            _day08.FlattenLayers();
         }
     }
 }
