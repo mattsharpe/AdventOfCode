a679b413b60e38b100ea280f5dcecda886ad1bd8
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-11 20:44:21 +0000

    Using some trig to solve the targetting of asteroids

---
+++ Advent2019/Solutions/Day10.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day10
+    {
+        private readonly HashSet<(int x, int y)> _asteroids = new HashSet<(int x, int y)>();
+
+        public (int visibleAsteroids, int x, int y) BestLocationForStation(string[] input)
+        {
+            //build a list of asteroids co-ordinates
+            for (var y = 0; y < input.Length; y++)
+            {
+                var line = input[y];
+                for (var x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == '#')
+                        _asteroids.Add((x, y));
+                }
+            }
+
+            return _asteroids.Select(asteroid =>
+                {
+                    var count = NumberOfAsteroidsInSight(asteroid);
+                    return (count, asteroid.x, asteroid.y);
+                }).OrderByDescending(x => x.count)
+                .First();
+        }
+
+        public int NumberOfAsteroidsInSight((int x, int y) station)
+        {
+            var angles = new HashSet<double>();
+            foreach (var target in _asteroids)
+            {
+                if (station.x != target.x || station.y != target.y)
+                {
+                    angles.Add(Math.Atan2(target.y - station.y, target.x - station.x));
+                }
+            }
+
+            return (angles.Count);
+        }
+
+        public List<(int x, int y)> FireTheLaserPewPew((int x, int y) station)
+        {
+            List<(int X, int Y, double angle, double distance)> targets = _asteroids.Select(target => 
+                (
+                    target.x, 
+                    target.y,
+                    Math.Atan2(target.y - station.y, target.x - station.x) * (180 / Math.PI), //convert rads to degrees
+                    Math.Sqrt(Math.Pow(station.x - target.x, 2) + Math.Pow(station.y - target.y, 2)) //distance between them
+                )).OrderBy(x=>x.Item3).ThenBy(x=>x.Item4).ToList();
+            
+            //get unique list of angles we need to hit
+            var anglesToAttack = targets.Select(x => x.angle).ToHashSet().ToList();
+            var currentAngleId = anglesToAttack.IndexOf(-90d);
+            var destroyed = new List<(int x, int y)>();
+            
+            while (targets.Any())
+            {
+                var nextAngle = anglesToAttack[currentAngleId];
+                if(targets.Exists(x => x.angle.Equals(nextAngle)))
+                {
+                    var targetAsteroid = targets.First(x => x.angle.Equals(nextAngle));
+                    targets.Remove(targetAsteroid);
+                    destroyed.Add((targetAsteroid.X,targetAsteroid.Y));
+                }
+                currentAngleId = currentAngleId < anglesToAttack.Count - 1 ? currentAngleId + 1 : 0;
+            }
+
+            return destroyed;
+        }
+    }
+}
---
+++ Advent2019/Tests/Day10Tests.cs
@@ -0,0 +1,193 @@
+﻿using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        private Day10 _day10;
+
+        [TestInitialize]
+        public void Initialize() => _day10 = new Day10();
+
+        [TestMethod]
+        public void SmallMap()
+        {
+            var input = new[]
+            {
+                ".#..#",
+                ".....",
+                "#####",
+                "....#",
+                "...##",
+            };
+
+            var result = _day10.BestLocationForStation(input);
+            Assert.AreEqual(result, (8, 3, 4));
+        }
+
+        [TestMethod]
+        public void Detects33AsteroidsAtX5Y8()
+        {
+            var input = new[]
+            {
+                "......#.#.",
+                "#..#.#....",
+                "..#######.",
+                ".#.#.###..",
+                ".#..#.....",
+                "..#....#.#",
+                "#..#....#.",
+                ".##.#..###",
+                "##...#..#.",
+                ".#....####",
+            };
+
+            var result = _day10.BestLocationForStation(input);
+            Assert.AreEqual(result, (33, 5, 8));
+        }
+
+        [TestMethod]
+        public void Detects35AsteroidsAtX1Y2()
+        {
+            var input = new[]
+            {
+                "#.#...#.#.",
+                ".###....#.",
+                ".#....#...",
+                "##.#.#.#.#",
+                "....#.#.#.",
+                ".##..###.#",
+                "..#...##..",
+                "..##....##",
+                "......#...",
+                ".####.###.",
+            };
+
+            var result = _day10.BestLocationForStation(input);
+            Assert.AreEqual(result, (35, 1, 2));
+        }
+
+        [TestMethod]
+        public void Detects41AsteroidsAtX6Y3()
+        {
+            var input = new[]
+            {
+                ".#..#..###",
+                "####.###.#",
+                "....###.#.",
+                "..###.##.#",
+                "##.##.#.#.",
+                "....###..#",
+                "..#.#..#.#",
+                "#..#.#.###",
+                ".##...##.#",
+                ".....#.#.."
+            };
+
+            var result = _day10.BestLocationForStation(input);
+            Assert.AreEqual(result, (41, 6, 3));
+        }
+
+        [TestMethod]
+        public void Detects210AsteroidsAtX11Y13()
+        {
+            var input = new[]
+            {
+                ".#..##.###...#######",
+                "##.############..##.",
+                ".#.######.########.#",
+                ".###.#######.####.#.",
+                "#####.##.#.##.###.##",
+                "..#####..#.#########",
+                "####################",
+                "#.####....###.#.#.##",
+                "##.#################",
+                "#####.##.###..####..",
+                "..######..##.#######",
+                "####.##.####...##..#",
+                ".#####..#.######.###",
+                "##...#.##########...",
+                "#.##########.#######",
+                ".####.#.###.###.#.##",
+                "....##.##.###..#####",
+                ".#.#.###########.###",
+                "#.#.#.#####.####.###",
+                "###.##.####.##.#..##",
+            };
+
+            var result = _day10.BestLocationForStation(input);
+            Assert.AreEqual(result, (210, 11, 13));
+        }
+
+        [TestMethod]
+        public void FindBestLocationForAsteroidStation()
+        {
+            var input = FileReader.ReadFile("day10.txt");
+            Assert.AreEqual((344, 30,34), _day10.BestLocationForStation(input));
+
+        }
+
+        [TestMethod]
+        public void RunAsteroidVaporizationSimulation()
+        {
+            var asteroidField = new []
+            {
+                ".#....#####...#..",
+                "##...##.#####..##",
+                "##...#...#.#####.",
+                "..#.....#...###..",
+                "..#.#.....#....##",
+            };
+            Assert.AreEqual((30,8,3), _day10.BestLocationForStation(asteroidField));
+            
+            _day10.FireTheLaserPewPew((8,3));
+        }
+
+        [TestMethod]
+        public void RunAsteroidVaporizationSimulationLargeSample()
+        {
+            var asteroidField = new[]
+            {
+                ".#..##.###...#######",
+                "##.############..##.",
+                ".#.######.########.#",
+                ".###.#######.####.#.",
+                "#####.##.#.##.###.##",
+                "..#####..#.#########",
+                "####################",
+                "#.####....###.#.#.##",
+                "##.#################",
+                "#####.##.###..####..",
+                "..######..##.#######",
+                "####.##.####...##..#",
+                ".#####..#.######.###",
+                "##...#.##########...",
+                "#.##########.#######",
+                ".####.#.###.###.#.##",
+                "....##.##.###..#####",
+                ".#.#.###########.###",
+                "#.#.#.#####.####.###",
+                "###.##.####.##.#..##",
+            };
+            Assert.AreEqual((210,11,13), _day10.BestLocationForStation(asteroidField));
+            
+            var (x, y) = _day10.FireTheLaserPewPew((11,13))[199];
+            
+            Assert.AreEqual(802, x * 100 + y);
+        }
+
+
+        [TestMethod]
+        public void FireZeLasers()
+        {
+            var asteroidField = FileReader.ReadFile("day10.txt");
+            Assert.AreEqual((344,30,34), _day10.BestLocationForStation(asteroidField));
+            var (x, y) = _day10.FireTheLaserPewPew((30,34))[199];
+            
+            Assert.AreEqual(2732, x * 100 + y);
+        }
+    }
+}
