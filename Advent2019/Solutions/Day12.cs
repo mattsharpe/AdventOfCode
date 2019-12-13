56373047bee515ed2d25eae58cc2b30d85c56d7e
mattsharpe <mattsharpe@users.noreply.github.com>
2019-12-13 22:15:21 +0000

    The n-Body problem, identifying where moons of Jupiter will orbit (#40)
    * Part 1 complete
    
    * Updated test with correct answer for part 1
    
    * Part 2 of day 12 completed

---
+++ Advent2019/Solutions/Day12.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2019.Solutions
+{
+    class Day12
+    {
+        public List<Moon> Moons { get; set; } = new List<Moon>();
+
+        public void ParseInput(string[] input)
+        {
+            Moons = input.Select(moon => Regex.Match(moon, @"x=(-?\d+), y=(-?\d+), z=(-?\d+)"))
+                .Select(match => new Moon
+                {
+                    Position = (
+                    Convert.ToInt32(match.Groups[1].Value),
+                    Convert.ToInt32(match.Groups[2].Value),
+                    Convert.ToInt32(match.Groups[3].Value)
+                )
+                }).ToList();
+        }
+
+        public void Step()
+        {
+            // Apply gravity
+            foreach (var moon in Moons)
+            {
+                var toCompare = Moons.Where(x => x != moon).ToArray();
+
+                var velocityX = moon.Velocity.x +
+                                toCompare.Count(a => a.Position.x > moon.Position.x) -
+                                toCompare.Count(a => a.Position.x < moon.Position.x);
+
+                var velocityY = moon.Velocity.y +
+                                toCompare.Count(a => a.Position.y > moon.Position.y) -
+                                toCompare.Count(a => a.Position.y < moon.Position.y);
+
+                var velocityZ = moon.Velocity.z + 
+                                toCompare.Count(a => a.Position.z > moon.Position.z) -
+                                toCompare.Count(a => a.Position.z < moon.Position.z);
+
+                moon.Velocity = (velocityX, velocityY, velocityZ);
+
+            }
+
+            foreach (var moon in Moons)
+            {
+                moon.Position = (moon.Position.x + moon.Velocity.x,
+                                moon.Position.y + moon.Velocity.y,
+                                moon.Position.z + moon.Velocity.z);
+            }
+
+        }
+
+        public int CalculateTotalEnergy()
+        {
+            return Moons.Sum(moon =>
+                (Math.Abs(moon.Velocity.x) + Math.Abs(moon.Velocity.y) + Math.Abs(moon.Velocity.z)) *
+                (Math.Abs(moon.Position.x) + Math.Abs(moon.Position.y) + Math.Abs(moon.Position.z))
+            );
+        }
+
+        public long NumberOfStepsUntilRepeatedState()
+        {
+            var xPeriod = -1;
+            var yPeriod = -1;
+            var zPeriod = -1;
+
+            int count =0;
+            while (xPeriod == -1 || yPeriod == -1 || zPeriod == -1)
+            {
+                Step();
+                count++;
+
+                if (xPeriod == -1 && Moons.All(moon => moon.Velocity.x == 0))
+                    xPeriod = count;
+                
+                if (yPeriod == -1 && Moons.All(moon => moon.Velocity.y == 0))
+                    yPeriod = count;
+                
+                if (zPeriod == -1 && Moons.All(moon => moon.Velocity.z == 0))
+                    zPeriod = count;
+
+            }
+
+            return LowestCommonMultiple(xPeriod, LowestCommonMultiple(yPeriod, zPeriod)) *2;
+        }
+
+        private long GreatestCommonDivisor(long a, long b)
+        {
+            if (a % b == 0) return b;
+            return GreatestCommonDivisor(b, a % b);
+        }
+
+        private long LowestCommonMultiple(long a, long b)
+        {
+            return a * b / GreatestCommonDivisor(a, b);
+        }
+
+    }
+
+    public class Moon
+    {
+        public (int x, int y, int z) Position { get; set; }
+        public (int x, int y, int z) Velocity { get; set; }
+    }
+}
---
+++ Advent2019/Tests/Day12Tests.cs
@@ -0,0 +1,180 @@
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        private Day12 _day12;
+
+        [TestInitialize]
+        public void Initialize() => _day12 = new Day12();
+
+        [TestMethod]
+        public void SingleStep()
+        {
+            string[] input =
+            {
+                "<x=-1, y=0, z=2>",
+                "<x=2, y=-10, z=-7>",
+                "<x=4, y=-8, z=8>",
+                "<x=3, y=5, z=-1>"
+            };
+
+            _day12.ParseInput(input);
+
+            _day12.Step();
+
+            Assert.AreEqual(_day12.Moons[0].Position, (2,-1,1));
+            Assert.AreEqual(_day12.Moons[0].Velocity, (3,-1,-1));
+
+            Assert.AreEqual(_day12.Moons[1].Position, (3, -7, -4));
+            Assert.AreEqual(_day12.Moons[1].Velocity, (1,3,3));
+            
+            Assert.AreEqual(_day12.Moons[2].Position, (1,-7,5));
+            Assert.AreEqual(_day12.Moons[2].Velocity, (-3,1,-3));
+            
+            Assert.AreEqual(_day12.Moons[3].Position, (2,2,0));
+            Assert.AreEqual(_day12.Moons[3].Velocity, (-1,-3,1));
+        }
+
+        [TestMethod]
+        public void TenSteps()
+        {
+            string[] input =
+            {
+                "<x=-1, y=0, z=2>",
+                "<x=2, y=-10, z=-7>",
+                "<x=4, y=-8, z=8>",
+                "<x=3, y=5, z=-1>"
+            };
+
+            _day12.ParseInput(input);
+
+            foreach (var irrelevant in Enumerable.Range(1,10))
+            {
+                _day12.Step();
+            }
+            
+            Assert.AreEqual(_day12.Moons[0].Position, (2,1,-3));
+            Assert.AreEqual(_day12.Moons[0].Velocity, (-3,-2,1));
+
+            Assert.AreEqual(_day12.Moons[1].Position, (1,-8,0));
+            Assert.AreEqual(_day12.Moons[1].Velocity, (-1,1,3));
+            
+            Assert.AreEqual(_day12.Moons[2].Position, (3,-6,1));
+            Assert.AreEqual(_day12.Moons[2].Velocity, (3,2,-3));
+            
+            Assert.AreEqual(_day12.Moons[3].Position, (2,0,4));
+            Assert.AreEqual(_day12.Moons[3].Velocity, (1,-1,-1));
+
+        }
+
+        [TestMethod]
+        public void TotalEnergyAfter10Steps()
+        {
+            string[] input =
+            {
+                "<x=-1, y=0, z=2>",
+                "<x=2, y=-10, z=-7>",
+                "<x=4, y=-8, z=8>",
+                "<x=3, y=5, z=-1>"
+            };
+
+            _day12.ParseInput(input);
+
+            foreach (var irrelevant in Enumerable.Range(1,10))
+            {
+                _day12.Step();
+            }
+
+            Assert.AreEqual(179,_day12.CalculateTotalEnergy());
+        }
+
+        [TestMethod]
+        public void TotalEnergyAfter10StepsSecond()
+        {
+            string[] input =
+            {
+                "<x=-8, y=-10, z=0>",
+                "<x=5, y=5, z=10>",
+                "<x=2, y=-7, z=3>",
+                "<x=9, y=-8, z=-3>"
+            };
+
+            _day12.ParseInput(input);
+
+            foreach (var irrelevant in Enumerable.Range(1,100))
+            {
+                _day12.Step();
+            }
+
+            Assert.AreEqual(1940, _day12.CalculateTotalEnergy());
+        }
+
+        [TestMethod]
+        public void EnergyAfter1000Steps()
+        {
+            var input = FileReader.ReadFile("day12.txt");
+
+            _day12.ParseInput(input);
+
+            foreach (var irrelevant in Enumerable.Range(1,1000))
+            {
+                _day12.Step();
+            }
+
+            Assert.AreEqual(7013, _day12.CalculateTotalEnergy());
+
+        }
+
+        [TestMethod]
+        public void HowLongUntilRepetition()
+        {
+            string[] input =
+            {
+                "<x=-1, y=0, z=2>",
+                "<x=2, y=-10, z=-7>",
+                "<x=4, y=-8, z=8>",
+                "<x=3, y=5, z=-1>"
+            };
+
+            _day12.ParseInput(input);
+
+            Assert.AreEqual(2772, _day12.NumberOfStepsUntilRepeatedState());
+
+        }
+
+
+        [TestMethod]
+        public void HowLongUntilRepetitionLarge()
+        {
+            string[] input =
+            {
+                "<x=-8, y=-10, z=0>",
+                "<x=5, y=5, z=10>",
+                "<x=2, y=-7, z=3>",
+                "<x=9, y=-8, z=-3>"
+            };
+            
+            _day12.ParseInput(input);
+
+            Assert.AreEqual(4686774924, _day12.NumberOfStepsUntilRepeatedState());
+        }
+
+        [TestMethod]
+        public void HowLongUntilRepetitionPart2()
+        {
+            string[] input = FileReader.ReadFile("day12.txt");
+            
+            _day12.ParseInput(input);
+
+            Assert.AreEqual(324618307124784, _day12.NumberOfStepsUntilRepeatedState());
+        }
+
+    }
+
+}
