e4dc7ea44799d5dfd0d61791f3e09a49599ab9db
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-13 10:28:59 +0000

    Building an emergency paiting robot to put the ship's name on the hull to avoid the space police

---
+++ Advent2019/Tests/Day11Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        private Day11 _robot;
+
+        [TestInitialize]
+        public void Initialize() => _robot = new Day11();
+
+        [TestMethod]
+        public void CountPaintedSquares()
+        {
+            var program = FileReader.ReadFile("day11.txt").First();
+            _robot.Initialize(program);
+            _robot.DrawRegistration();
+
+            Assert.AreEqual(2093, _robot.PaintedSquares());
+        }
+
+        [TestMethod]
+        public void PaintShipName()
+        {
+            var program = FileReader.ReadFile("day11.txt").First();
+            _robot.Initialize(program);
+            _robot.AddInput(1);
+            _robot.DrawRegistration();
+            _robot.Print();
+            
+        }
+    }
+
+}
