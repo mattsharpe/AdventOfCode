281b07f139be97dce7c506be1e14d29a2f1ea93a
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-13 22:39:26 +0000

    Day13 started, part 2 looks like it will require a very different approach though

---
+++ Advent2019/Solutions/Day13.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2019.Solutions
+{
+    class Day13
+    {
+        private IntCodeComputer _computer =new IntCodeComputer();
+        
+        public int CountNumberOfBlocks(string program)
+        {
+            _computer.InitializePositions(program);
+            var task = Task.Run(() => _computer.RunProgram());
+
+            task.Wait();
+            var blocks = new List<long>();
+            var output = new List<long>(_computer.Outputs);
+            for(var i=2; i <output.Count; i += 3)
+            {
+               blocks.Add(output[i]);
+            }
+
+            return blocks.Count(x=>x == 2);
+            
+        }
+    }
+}
---
+++ Advent2019/Tests/Day13Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        private Day13 _day13 = new Day13();
+
+        [TestInitialize]
+        public void Initialize() => _day13 = new Day13();
+
+        [TestMethod]
+        public void RunGame()
+        {
+            var program = FileReader.ReadFile("day13.txt").First();
+            Assert.AreEqual(226, _day13.CountNumberOfBlocks(program));
+        }
+    }
+}
