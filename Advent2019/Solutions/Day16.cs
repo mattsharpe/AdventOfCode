8aac48aa4bdc57554efa3fe0163161c581605432
Matt Sharpe <matthew.sharpe@sword-grc.com>
2019-12-31 16:17:47 +0000

    Flawed Frequency Transmission Part 1. Using a repeating sequence and the dot product of the input number

---
+++ Advent2019/Solutions/Day16.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2019.Solutions
+{
+    class Day16
+    {
+        public string ProcessPhase(string input)
+        {
+            var numbers = input.Select(x=> Convert.ToInt32(x.ToString())).ToArray();
+            var result = new List<int>();
+
+            foreach (var digit in Enumerable.Range(0, input.Length))
+            {
+                var pattern = Pattern(digit).Skip(1);
+                var dotProduct = numbers.Zip(pattern, (x1, x2) => x1 * x2).Sum();
+                result.Add(Math.Abs(dotProduct) % 10);
+            }
+
+            return string.Join("", result);
+        }
+
+
+        private static IEnumerable<int> Pattern(int length)
+        {
+            //have to skip the first number so generate one more than asked for.
+            length++;
+            var sequence = new[] {0, 1, 0, -1};
+            while (true)
+            {
+                foreach (var item in sequence)
+                {
+                    foreach (var count in Enumerable.Range(0, length))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+
+        public string RunPhases(string input, int count)
+        {
+            foreach (var i in Enumerable.Range(0,count))
+            {
+                input = ProcessPhase(input);    
+            }
+
+            return input.Substring(0,8);
+        }
+        
+    }
+}
---
+++ Advent2019/Tests/Day16Tests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Advent2019.Solutions;
+using Advent2019.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2019.Tests
+{
+    [TestClass]
+    public class Day16Tests
+    {
+        private Day16 _day16;
+
+        [TestInitialize]
+        public void Initialize() => _day16 = new Day16();
+
+        [DataTestMethod]
+        [DataRow("12345678", 1, "48226158")]
+        [DataRow("12345678", 2, "34040438")]
+        [DataRow("12345678", 3, "03415518")]
+        [DataRow("12345678", 4, "01029498")]
+        public void Samples(string input, int phases, string expected)
+        {
+            Assert.AreEqual(expected, _day16.RunPhases(input, phases));
+        }
+
+        [TestMethod]
+        [DataRow("80871224585914546619083218645595","24176176")]
+        [DataRow("19617804207202209144916044189917","73745418")]
+        [DataRow("69317163492948606335995924319873","52432133")]
+        public void LargerExamples(string input, string expected)
+        {
+            Assert.AreEqual(expected, _day16.RunPhases(input, 100));
+        }
+
+        [TestMethod]
+        public void DotProduct()
+        {
+            var input = new[] {9, 8, 7, 6, 5};
+            var pattern = new[] {1,2,3,1,2};
+
+            var result = input.Zip(pattern, (x1, x2) => x1 * x2).Sum();
+            Assert.AreEqual(62, result);
+            Assert.AreEqual(2, result % 10);
+        }
+
+        [TestMethod]
+        public void FlawedFrequencyTransmission()
+        {
+            var input = FileReader.ReadFile("day16.txt").First();
+            Assert.AreEqual("96136976",_day16.RunPhases(input,100));
+        }
+    }
+}
