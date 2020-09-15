7a970a269c33059f210cd6ff5fe7d82cce3546df
mattsharpe <mattsharpe@users.noreply.github.com>
2020-09-15 15:57:39 +0100

    2015 Day 20 - Helping the elves deliver presents to houses (#46)
    Co-authored-by: Matthew Sharpe <matthew.sharpe@sword-grc.com>

---
+++ Advent2015/Solutions/Day20.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Advent2015.Solutions
+{
+    class Day20
+    {
+        public int Solve(int input)
+        {
+            var houses = new int[1000000];
+
+            for (var i = 1; i < houses.Length; i++) 
+            {
+                for(var j = i; j< houses.Length; j+= i) 
+                {
+                    houses[j] += i * 10;
+                }
+            }
+
+            for (var i = 0; i < houses.Length; i++) {
+                if (houses[i] >= input) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int Part2(int input)
+        {
+            var houses = new int[1000000];
+
+            for (var i = 1; i < houses.Length; i++) {
+                var housesVisited = 0;
+            
+                for(var j = i; j< houses.Length; j+= i)
+                {
+                    if (housesVisited == 50) break;
+                    houses[j] += i * 11;
+                    housesVisited++;
+                }
+            
+            }
+
+            for (var i = 0; i < houses.Length; i++) {
+                if (houses[i] >= input) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
