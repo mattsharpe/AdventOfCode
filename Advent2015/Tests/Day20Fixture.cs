45fa5900ff3c25ea5c6e3963c3b5192b1795295b
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-09-15 15:51:53 +0100

    2015 Day 20 - Helping the elves deliver presents to houses

---
+++ Advent2015/Tests/Day20Fixture.cs
@@ -0,0 +1,30 @@
+﻿using Advent2015.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    
+    [TestClass]
+    public class Day20Fixture
+    {
+        private Day20 _day20;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day20 = new Day20();
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(786240, _day20.Solve(34000000));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(831600, _day20.Part2(34000000));
+        }
+    }
+}
