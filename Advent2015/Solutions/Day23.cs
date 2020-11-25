0d0d40fcadb3cd3902d20cd49709c820e4606b4c
Matt Sharpe <matt@mattsharpe.co.uk>
2020-11-25 13:19:06 +0000

    Processing instructions to interpret a program

---
+++ Advent2015/Solutions/Day23.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent2015.Solutions
+{
+    class Day23
+    {
+        public Dictionary<char, int> Registers = new Dictionary<char, int>
+        {
+            {'a', 0},
+            {'b', 0}
+        };
+
+        public int ProcessInstruction(string command)
+        {
+            
+            var split = command.Split(' ');
+            string instruction = split[0];
+            char register = split[1][0];
+
+            switch (instruction)
+            {
+                case "hlf": //hlf a
+                    Registers[register] /= 2;
+                    break;
+                case "tpl": //tpl a
+                    Registers[register] *= 3;
+                    break;
+                case "inc": //inc a
+                    Registers[register]++;
+                    break;
+                case "jmp": //jmp +19
+                    return Convert.ToInt32(split[1]);
+                case "jie": //jie a, +4
+                    return Registers[register] % 2 == 0 ? Convert.ToInt32(split[2]) : 0;
+                case "jio": //jie a, +4
+                    return Registers[register] == 1 ? Convert.ToInt32(split[2]) : 0;
+                default:
+                    throw new ArgumentException($"Unrecognised command {command}");
+            }
+
+            return 0;
+        }
+
+        public void ProcessInstructions(string[] input)
+        {
+            int position = 0;
+
+            while (position < input.Length)
+            {
+                var instruction = input[position];
+                var jump = ProcessInstruction(instruction);
+                if (jump == 0)
+                {
+                    position++;
+                }
+                else
+                {
+                    position += jump;
+                }
+            }
+        }
+
+
+    }
+}
