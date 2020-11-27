077544217f28e9cff1129c3cf0fe44e704ef3e33
Matt Sharpe <matt@mattsharpe.co.uk>
2020-11-27 18:11:48 +0000

    DAy 25 - 2015 complete

---
+++ Advent2015/Tests/Day25Fixture.cs
@@ -0,0 +1,27 @@
+﻿using Advent2015.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2015.Tests
+{
+    [TestClass]
+    public class Day25Fixture
+    {
+        private Day25 _day25;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day25 = new Day25();
+        }
+
+        [TestMethod]
+        public void Run()
+        {
+            //To continue, please consult the code grid in the manual.  Enter the code at row 3010, column 3019.
+            var result = _day25.Solve(3010, 3019);
+            Assert.AreEqual(8997277, result);
+        }
+        
+
+    }
+}
