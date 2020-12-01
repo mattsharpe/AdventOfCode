324d05e2aeefddee94cc8624ce31bbc7f86628da
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-01 09:05:31 +0000

    Day 1 - sorting through some star coins to find groups that sum to a specific value

---
+++ Advent2020/Solutions/Day01.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day01
+    {
+        public int ProductOfSum(int[] input)
+        {
+            var result = from x in input
+                from y in input
+                where x + y == 2020
+                select x * y;
+
+            return result.First();
+        }
+
+        public int GroupOfThree(int[] input)
+        {
+            var result = from x in input
+                from y in input
+                from z in input
+                where x + y + z == 2020
+                select x * y * z;
+
+            return result.First();
+        }
+    }
+}
---
+++ Advent2020/Tests/Day01Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        private Day01 _day01;
+        private int[] _sample;
+        private int[] _input;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day01 = new Day01();
+            _sample = new[] {1721, 979, 366, 299, 675, 1456};
+            _input = File.ReadAllLines("Input/Day01.txt").Select(x => Convert.ToInt32(x)).ToArray();
+        }
+
+        [TestMethod]
+        public void Sample()
+        {
+            var result = _day01.ProductOfSum(_sample);
+            Assert.AreEqual(514579, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = _day01.ProductOfSum(_input);
+
+            Assert.AreEqual(471019, result);
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            var result = _day01.GroupOfThree(_sample);
+
+            Assert.AreEqual(241861950, result);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var result = _day01.GroupOfThree(_input);
+
+            Assert.AreEqual(103927824, result);
+        }
+    }
+
+
+}
