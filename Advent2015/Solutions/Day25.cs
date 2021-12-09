d5366f70b8c56fac3eb5b8705b15d995b2f7dd33
mattsharpe <mattsharpe@users.noreply.github.com>
2021-12-09 08:31:50 +0000

    DAy 25 - 2015 complete (#52)

---
+++ Advent2015/Solutions/Day25.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+namespace Advent2015.Solutions
+{
+    class Day25
+    {
+        public long Solve(int row, int column)
+        {
+            long a = 20151125;
+            int x = 1;
+            int y = 1;
+
+            while (x != row || y != column)
+            {
+                x--;
+                y++;
+                if (x == 0)
+                {
+                    x = y;
+                    y = 1;
+                }
+                a = (a * 252533L) % 33554393L;
+            }
+
+            return a;
+        }
+
+    }
+}
