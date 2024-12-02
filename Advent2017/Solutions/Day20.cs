54a44a4ed6e24468ffe2d603176bee18ac9f9248
Matt Sharpe <matthew.sharpe@riskonnect.com>
2024-12-02 20:46:17 +0000

    --- Day 20: Particle Swarm ---

---
+++ Advent2017/Solutions/Day20.cs
@@ -0,0 +1,78 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Advent2017.Solutions
+{
+    internal class Day20
+    {
+
+        public int Part1(string[] input)
+        {
+            var particles = ParseInput(input);
+
+            return particles.OrderBy(p => p.Acceleration.LengthSquared())
+                            .ThenBy(p => p.Velocity.LengthSquared())
+                            .ThenBy(p => p.Position.LengthSquared())
+                            .First().Id;
+        }
+
+        public int Part2(string[] input) {
+
+            var particles = ParseInput(input);
+            SimulateParticlesWithCollisions(particles, 1000);
+            return particles.Count;
+        }
+
+        public List<Particle> ParseInput(string[] input)
+        {
+            return Enumerable.Zip(input, Enumerable.Range(0, input.Length), ParseLine).ToList();
+        }
+
+        public Particle ParseLine(string line, int id)
+        {
+            var numbers = Regex.Matches(line, "-?\\d+").Select(x => int.Parse(x.Value)).ToArray();
+
+            return new Particle(id,
+                            new Vector3(numbers[0], numbers[1], numbers[2]),
+                            new Vector3(numbers[3], numbers[4], numbers[5]),
+                            new Vector3(numbers[6], numbers[7], numbers[8]));
+        }
+
+        private void SimulateParticlesWithCollisions(List<Particle> particles, int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                var positionMap = new Dictionary<Vector3, List<Particle>>();
+
+                foreach (var particle in particles)
+                {
+                    particle.Velocity += particle.Acceleration;
+                    particle.Position += particle.Velocity;
+
+                    if (!positionMap.TryGetValue(particle.Position, out List<Particle> value))
+                    {
+                        value = ([]);
+                        positionMap[particle.Position] = value;
+                    }
+
+                    value.Add(particle);
+                }
+
+                foreach (var group in positionMap.Values.Where(g => g.Count > 1))
+                {
+                    foreach (var particle in group)
+                    {
+                        particles.Remove(particle);
+                    }
+                }
+            }
+        }
+    }
+
+    record Particle(int Id, Vector3 Position, Vector3 Velocity, Vector3 Acceleration)
+    {
+        public Vector3 Position { get; set; } = Position;
+        public Vector3 Velocity { get; set; } = Velocity;
+        public Vector3 Acceleration { get; set; } = Acceleration;
+    }
+}
---
+++ Advent2017/Tests/Day20Fixture.cs
@@ -0,0 +1,55 @@
+﻿using Advent2017.Solutions;
+using Advent2017.Utilities;
+
+namespace Advent2017.Tests
+{
+    [TestClass]
+    public class Day20Fixture
+    {
+        private readonly string[] _sample = [
+            "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>",
+            "p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>",
+        ];
+
+        private readonly string[] _sample2 = [
+            "p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>",
+            "p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>",
+            "p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>",
+            "p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>",
+        ];
+
+        private string[] _input = FileReader.ReadFile("day20.txt");
+
+        private Day20 _day = new();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day = new Day20();
+        }
+
+        [TestMethod]
+        public void Part1Sample()
+        {
+            Assert.AreEqual(0, _day.Part1(_sample));
+        }
+        
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(91, _day.Part1(_input));
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            Assert.AreEqual(1, _day.Part2(_sample2));
+        }
+        
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(567, _day.Part2(_input));
+        }
+    }
+}
