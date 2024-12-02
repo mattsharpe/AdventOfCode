06e47d1129996b7335e4c807f57027aa042f6009
Matt Sharpe <matthew.sharpe@riskonnect.com>
2024-12-02 17:27:45 +0000

    --- Day 19: A Series of Tubes ---

---
+++ Advent2017/Solutions/Day19.cs
@@ -0,0 +1,78 @@
+﻿using System.Numerics;
+
+namespace Advent2017.Solutions
+{
+    class Day19
+    {
+        public string Part1(string[] input)
+        {
+            var map = ParseInput(input);
+            var solver = new NetworkPacketSolver(map);
+            return solver.Solve().Letters;
+        }
+
+        public int Part2(string[] input)
+        {
+            var map = ParseInput(input);
+            var solver = new NetworkPacketSolver(map);
+            return solver.Solve().Steps;
+        }
+
+        public Dictionary<Vector2, string> ParseInput(string[] input)
+        {
+            Dictionary<Vector2, string> map = new Dictionary<Vector2, string>();
+
+            foreach (var y in Enumerable.Range(0, input.Length))
+                foreach (var x in Enumerable.Range(0, input[0].Length))
+                {
+                    map.Add(new Vector2(x, y), input[y][x].ToString());
+                }
+            return map;
+        }
+    }
+
+    class NetworkPacketSolver(Dictionary<Vector2, string> map)
+    {
+        private Vector2 _position = map.Single(kvp => kvp.Key.Y == 0 && kvp.Value == "|").Key;
+        private Vector2 _direction = new Vector2(0, 1);
+        private string _letters = "";
+        private int _steps = 0;
+
+        public (string Letters, int Steps) Solve()
+        {
+            while (true)
+            {
+                _position += _direction;
+                _steps++;
+
+                if (!map.TryGetValue(_position, out string value) || value == " ")
+                    break;
+
+                var current = map[_position];
+
+                if (current == "+")
+                {
+                    if (_direction.X == 0)
+                    {
+                        if (map.ContainsKey(_position + new Vector2(1, 0)) && map[_position + new Vector2(1, 0)] != " ")
+                            _direction = new Vector2(1, 0);
+                        else if (map.ContainsKey(_position + new Vector2(-1, 0)) && map[_position + new Vector2(-1, 0)] != " ")
+                            _direction = new Vector2(-1, 0);
+                    }
+                    else
+                    {
+                        if (map.ContainsKey(_position + new Vector2(0, 1)) && map[_position + new Vector2(0, 1)] != " ")
+                            _direction = new Vector2(0, 1);
+                        else if (map.ContainsKey(_position + new Vector2(0, -1)) && map[_position + new Vector2(0, -1)] != " ")
+                            _direction = new Vector2(0, -1);
+                    }
+                }
+                else if (current != "|" && current != "-")
+                {
+                    _letters += current;
+                }
+            }
+            return (_letters, _steps);
+        }
+    }
+}
---
+++ Advent2017/Tests/Day19Fixture.cs
@@ -0,0 +1,53 @@
+﻿using Advent2017.Solutions;
+using Advent2017.Utilities;
+
+namespace Advent2017.Tests
+{
+    [TestClass]
+    public class Day19Fixture
+    {
+        private readonly string[] _sample = [
+            "     |          ",
+            "     |  +--+    ",
+            "     A  |  C    ",
+            " F---|----E|--+ ",
+            "     |  |  |  D ",
+            "     +B-+  +--+ "
+        ];
+
+
+        private string[] _input = FileReader.ReadFile("day19.txt");
+
+        private Day19 _day = new();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day = new Day19();
+        }
+
+        [TestMethod]
+        public void Part1Sample()
+        {
+            Assert.AreEqual("ABCDEF", _day.Part1(_sample));
+        }
+        
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual("MOABEUCWQS", _day.Part1(_input));
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            Assert.AreEqual(38, _day.Part2(_sample));
+        }
+        
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(18058, _day.Part2(_input));
+        }
+    }
+}
